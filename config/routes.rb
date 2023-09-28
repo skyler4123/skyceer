e116ebc4d10307daed05e4698274d3c20d7bf9eb
skyler <pkphung2001@gmail.com>
2023-09-28 17:43:09 +0700

    Add Healthcheck

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get "/up", to: Proc.new { [200, {}, ["OK"]] }
+  root "users#index"
 end
