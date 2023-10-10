c65a890bdd66a51bddd5df0cbcfd866c43606859
skyler <pkphung2001@gmail.com>
2023-10-10 21:38:57 +0700

    Update

--- Dockerfile
+++ Dockerfile
@@ -59,5 +59,5 @@ ENTRYPOINT ["/rails/bin/docker-entrypoint"]
 
 # Start the server by default, this can be overwritten at runtime
 EXPOSE 3000
-# CMD ["./bin/rails", "server", "-b", "0.0.0.0"]
-CMD ["./bin/bundle", "exec", "sidekiq"]
+CMD ["./bin/rails", "server", "-b", "0.0.0.0"]
+# CMD ["./bin/bundle", "exec", "sidekiq"]
--- lib/websockets/websocket.rb
+++ lib/websockets/websocket.rb
@@ -3,7 +3,7 @@
 EM.run {
   @channels = {} # {channel_id: [ws]}
   @clients = {} # {client_id: channel_id}
-  EM::WebSocket.run(:host => "0.0.0.0", :port => 3000) do |ws|
+  EM::WebSocket.run(:host => "0.0.0.0", :port => 8080) do |ws|
     ws.onopen { |handshake|
     # TODO: validate
       puts "WebSocket connection open"
