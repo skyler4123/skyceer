0e506b7701c322936f345b3dd902f461b3bed958
skyler <pkphung2001@gmail.com>
2023-10-11 15:21:45 +0700

    Update

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,1 @@
-REDIS_HOST = ENV.fetch('PRIVATE_REDIS_HOST') { '0.0.0.0' }
-REDIS_PORT = ENV.fetch('REDIS_PORT') { '6379' }
-REDIS = Redis.new(url: "redis://" + REDIS_HOST + ":" + REDIS_PORT)
+REDIS = Redis.new(url: ENV.fetch('REDIS_URL') { 'redis://0.0.0.0:6379' })
