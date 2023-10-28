50cfd05c40f811e59b09ff4a49f12a02415cf433
skyler <pkphung2001@gmail.com>
2023-10-28 22:53:10 +0700

    Update

--- app/components/editor/trix_component.rb
+++ app/components/editor/trix_component.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class RichText::TrixComponent < ViewComponent::Base
+class Editor::TrixComponent < ViewComponent::Base
   def initialize(id: "trix-editor-#{SecureRandom.alphanumeric}", name:, editor_class: nil)
     @id = id
     @name = name.to_s
