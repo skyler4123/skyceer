9160d3923f144192d2542bb57db593c48b4d1a38
skyler <pkphung2001@gmail.com>
2023-11-05 14:06:38 +0700

    Test Sidekiq-Rspec test

--- spec/sidekiq/rock/hard_job_spec.rb
+++ spec/sidekiq/rock/hard_job_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 RSpec.describe Rock::HardJob, type: :job do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "should enqueue successfully" do
+    expect { Rock::HardJob.perform_async }.to change(Rock::HardJob.jobs, :size).by(1)
+  end
+  it "should increase User+1" do
+    Sidekiq::Testing.inline! do
+      expect { Rock::HardJob.perform_async }.to change(User, :count).by(1)
+    end
+  end
 end
