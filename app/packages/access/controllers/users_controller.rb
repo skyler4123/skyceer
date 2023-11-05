89c6c717ae2203c5ef1ad641a457eefd71bab70b
skyler <pkphung2001@gmail.com>
2023-11-05 10:58:36 +0700

    Update

--- app/packages/access/controllers/users_controller.rb
+++ app/packages/access/controllers/users_controller.rb
@@ -17,8 +17,8 @@ class UsersController < ApplicationController
   end
 
   # GET /users/1/edit
-  def edit
-  end
+  # def edit
+  # end
 
   # POST /users or /users.json
   def create
@@ -41,17 +41,17 @@ class UsersController < ApplicationController
   end
 
   # PATCH/PUT /users/1 or /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @user.update(user_params)
+  #       format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
+  #       format.json { render :show, status: :ok, location: @user }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @user.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /users/1 or /users/1.json
   def destroy
--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-  let!(:user) { create(:user) }
+  let(:user_attributes) { { email: 'email@gmail.com', password: 'password', password_confirmation: 'password' } }
+  subject!(:create_user) do
+    post users_url, params: { user: user_attributes }
+  end
+  let(:user) { User.find_by(email: user_attributes[:email]) }
+
   describe "GET /new" do
     it "returns http success" do
       get "/signin"
@@ -11,9 +16,41 @@ RSpec.describe "Sessions", type: :request do
 
   describe "POST /create" do
     it "returns http success" do
-      # User.create(email: 'email1@gmail.com', password: 'password', password_confirmation: 'password')
-      post "/signin", params: { email: user.email, password: 'password' }
+      post "/signin", params: { email: user.email, password: user_attributes[:password] }
       expect(response).to redirect_to(assigns(:user))
+    end
+
+    it "should successful due to access token expiration" do
+      post "/signin", params: { email: user.email, password: user_attributes[:password] }
+      travel_to (Constants::JWT_ACCESS_EXPIRATION.to_i).minutes.from_now do
+        get user_url(user)
+        expect(response).to render_template(:show)
+      end
+    end
+
+    it "should redirect to signin due to refesh token expiration" do
+      post "/signin", params: { email: user.email, password: user_attributes[:password] }
+      travel_to (Constants::JWT_REFRESH_EXPIRATION.to_i).minutes.from_now do
+        get user_url(user)
+        expect(response).to redirect_to(signin_path)
+      end
+    end
+
+    it "should redirect to signin because wrong password" do
+      post "/signin", params: { email: user.email, password: 'wrong_password' }
+      expect(response).to redirect_to(signin_path)
+    end
+
+    it "should redirect to signin because wrong email" do
+      post "/signin", params: { email: 'wrong_email@gmail.com', password: 'wrong_password' }
+      expect(response).to redirect_to(signin_path)
+    end
+  end
+
+  describe "DELETE /signout" do
+    it "returns signin after signout" do
+      delete "/signout"
+      expect(response).to redirect_to(signin_path)
     end
   end
 
