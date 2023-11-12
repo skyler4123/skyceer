d8b5ef641ee4c11b53b6b63431a89da6f932a7f6
skyler <pkphung2001@gmail.com>
2023-11-12 22:42:53 +0700

    Add Logo::Youtube

--- app/components/header/sticky_component.rb
+++ app/components/header/sticky_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Header::StickyComponent < ViewComponent::Base
-  def initialize(klass: "sticky top-0 shadow-xl")
+  def initialize(klass: "sticky top-0 shadow-xl bg-white")
     @klass = klass
   end
 end
--- app/components/logo/youtube_component.rb
+++ app/components/logo/youtube_component.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Logo::GithubComponent < ViewComponent::Base
+class Logo::YoutubeComponent < ViewComponent::Base
   def initialize(svg_class: "w-4 h-4 text-gray-500")
     @svg_class = svg_class
   end
