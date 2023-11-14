43e75cc0e47367b63c94b4beff459e345f7bb702
skyler <pkphung2001@gmail.com>
2023-11-14 07:05:51 +0700

    Update

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -4,7 +4,7 @@ class ButtonComponent < ViewComponent::Base
   renders_one :before
   renders_one :after
   
-  def initialize(label: 'Link', url: "#", button_class: "", link_class: "", klass: "")
+  def initialize(label: 'Button', url: "#", button_class: "", link_class: "", klass: "flex flex-row gap-x-2 bg-blue-700 hover:bg-blue-800 rounded-lg text-white font-medium py-3 px-5")
     @label = label
     @url = url
     @link_class = link_class
