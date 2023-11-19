2dd3a683229001aa5756303ddfb5b48507f6c739
skyler <pkphung2001@gmail.com>
2023-11-19 14:14:00 +0700

    Update

--- app/components/badge/pill_component.rb
+++ app/components/badge/pill_component.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Badge::PillComponent < ViewComponent::Base
-  renders_one :icon
+  renders_one :before
+  renders_one :after
 
   def initialize(label: "Badge", color: :blue)
     @label = label
