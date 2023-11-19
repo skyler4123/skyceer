ede8b6584a0a70f8db1c57fe06837ab4f2a72897
skyler <pkphung2001@gmail.com>
2023-11-19 21:30:01 +0700

    Update Indicator::Spinner

--- app/components/indicator/spinner_component.rb
+++ app/components/indicator/spinner_component.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class Indicator::SpinnerComponent < ViewComponent::Base
-
+  def initialize(svg_class: nil, color: :blue)
+    @svg_class = svg_class
+    @color = color.to_sym
+  end
 end
