91e201bfb90a80f708e67aa71ba2af4e143a732e
skyler <pkphung2001@gmail.com>
2023-11-20 20:38:08 +0700

    Update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def demo_text
-    render(TextComponent.new(klass: "flex flex-row justify-center items-center gap-x-1").with_after_content(render(Icon::MoonComponent.new)).with_before_content(render(Icon::SunComponent.new(svg_class: "w-4 h-4 text-yellow-500"))))
+    render(TextComponent.new(klass: "flex flex-row justify-center items-center gap-x-1").with_after_content(render(Icon::MoonComponent.new(svg_class: "w-4 h-4 animate-spin"))).with_before_content(render(Icon::SunComponent.new(svg_class: "w-4 h-4 text-yellow-500 animate-ping"))))
   end
 end
