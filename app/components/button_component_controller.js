af731ee0a3cfee6b7712ab04630bd7e50a78d2c6
skyler <pkphung2001@gmail.com>
2023-11-21 03:58:26 +0700

    Update

--- app/components/button_component_controller.js
+++ app/components/button_component_controller.js
@@ -1,7 +1,13 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["modal"]
+  static outlets = [""]
+  initialize() {
+    this.element.querySelector('[data-controller*=modal]').setAttribute('data-button-component-target', 'modal')
+  }
   connect() {
     // console.log("Hello, Stimulus!", this.element);
   }
+
 }
--- app/components/modal/default_component_controller.js
+++ app/components/modal/default_component_controller.js
@@ -2,11 +2,14 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   
+  open() {
+    this.element.classList.remove('hidden')
+  }
 
   exit() {
     // console.log(this.element.classList)
-    this.element.setAttribute('style', "display: none")
-    this.element.classList.remove('flex')
+    // this.element.setAttribute('style', "display: none")
+    // this.element.classList.remove('flex')
     this.element.classList.add('hidden')
 
   }
