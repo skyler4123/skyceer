579ab2868f1c60d0771271ffd81753f6e88d6dc3
skyler <pkphung2001@gmail.com>
2023-11-22 21:59:17 +0700

    Update Modal::Default

--- app/components/modal/default_component_controller.js
+++ app/components/modal/default_component_controller.js
@@ -3,10 +3,11 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
 
   close() {
-    console.log("Close from modal", this.element)
     this.element.classList.add('hidden')
     this.openOrCloseValue = "close"
+    setTimeout(() => {
     this.element.parentNode.closest('[data-controller]').setAttribute('data-action', 'click->button-component#openModal')
+    }, 500)
   }
 
 }
