122e98d40780d102b7d432530577c2e3750c42bb
skyler <pkphung2001@gmail.com>
2023-11-26 22:57:53 +0700

    Update

--- app/components/button/up_to_top_component_controller.js
+++ app/components/button/up_to_top_component_controller.js
@@ -2,6 +2,12 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
-    console.log("Hello, Stimulus!", this.element);
+    // console.log("Hello, Stimulus!", this.element);
+  }
+  initialize() {
+    this.element.setAttribute('data-action', `click->${this.identifier}#upToTop`)
+  }
+  upToTop() {
+    document.scrollingElement.scrollTo(0, 0)
   }
 }
