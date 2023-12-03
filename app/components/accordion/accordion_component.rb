d700fdf49a0f169a36c7e6eb2a9fd264a061afe6
skyler <pkphung2001@gmail.com>
2023-12-03 16:53:47 +0700

    Update

--- app/components/accordion/accordion_component.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class Accordion::AccordionComponent < ViewComponent::Base
-  renders_many :headers
-  renders_many :blocks
-end
--- app/components/accordion/accordion_component_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-  static targets = ["head"]
-
-  initialize() {
-    this.headTarget.classList.add('rounded-t-xl')
-  }
-}
