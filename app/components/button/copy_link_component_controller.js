8637b47c2c52638adf1836e47e70b53bade8c0a4
skyler <pkphung2001@gmail.com>
2023-12-06 03:09:03 +0700

    Update

--- app/components/button/copy_link_component_controller.js
+++ app/components/button/copy_link_component_controller.js
@@ -2,14 +2,26 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static values = {
-    url: String
+    url: String,
+    klass: { type: String, default: 'flex flex-row px-1 hover:cursor-pointer' }
   }
   connect() {
     // console.log("Hello, Stimulus!", this);
   }
   initialize() {
-    const url = this.element.parentNode.closest('[data-controller]').querySelector('a').href
-    this.urlValue = url
+    this.initializeKlass()
+    this.initializeUrl()
+    this.initializeAction()
+  }
+  initializeKlass() {
+    this.klassValue.split(' ').forEach((klass) => {
+      this.element.classList.add(klass)
+    })
+  }
+  initializeUrl() {
+    this.urlValue = this.element.parentNode.closest('[data-controller]').querySelector('a').href
+  }
+  initializeAction() {
     this.element.setAttribute('data-action', `click->${this.identifier}#copyUrl`)
   }
   copyUrl() {
