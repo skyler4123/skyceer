0e1b6dad8eb6aae8bed6dd65e2304e7698555fab
skyler <pkphung2001@gmail.com>
2023-12-06 02:30:40 +0700

    Update

--- app/components/button_component_controller.js
+++ app/components/button_component_controller.js
@@ -1,39 +1,43 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["modal"]
+  static targets = ["button", "link", "modal"]
   static values = {
-    klass: { type: String, default: "group peer flex flex-row gap-x-2 w-fit justify-center items-center bg-blue-700 hover:bg-blue-800 rounded-lg text-white font-medium p-3" },
+    klass: { type: String, default: "flex flex-row w-fit justify-center items-center cursor-pointer" },
     buttonClass: { type: String, default: "flex flex-row w-fit justify-center items-center" },
     linkClass: { type: String, default: "flex flex-row w-fit justify-center items-center" }
   }
 
   initialize() {
+    this.initializeTarget()
     this.initializeClass()
     this.initializeModal()
   }
   connect() {
     // console.log("Hello, Stimulus!", this.element);
   }
+  initializeTarget() {
+    this.element.querySelector('button').setAttribute(`data-${this.identifier}-target`, 'button')
+    this.element.querySelector('a')?.setAttribute(`data-${this.identifier}-target`, 'link')
+    this.element.querySelector('[data-controller*=modal]')?.setAttribute(`data-${this.identifier}-target`, 'modal')
+  }
   initializeClass() {
-    if (this.element.classList.length === 0) { this.element.classList = this.klassValue }
-    if (this.button()?.classList.length === 0) { this.button().classList = this.buttonClassValue }
-    if (this.link()?.classList.length === 0) { this.link().classList = this.linkClassValue }
-  }
-  button() {
-    return this.element.querySelector('button')
-  }
-  link() {
-    return this.element.querySelector('a')
+    this.klassValue.split(' ').forEach((klass) => {
+      this.element.classList.add(klass)
+    })
+    this.buttonClassValue.split(' ').forEach((klass) => {
+      this.buttonTarget.classList.add(klass)
+    })
+    this.hasLinkTarget && this.linkClassValue.split(' ').forEach((klass) => {
+      this.linkTarget.classList.add(klass)
+    })
+
   }
   initializeModal() {
-    if (this.element.querySelector('[data-controller*=modal]')) {
-      this.element.querySelector('[data-controller*=modal]').setAttribute('data-button-component-target', 'modal')
-      this.element.setAttribute('data-action', 'click->button-component#openModal')
-    }
+    this.hasModalTarget && this.modalTarget.setAttribute('data-action', `click->${this.identifier}#openModal`)
   }
   openModal() {
     this.modalTarget.classList.remove('hidden')
-    this.element.dataset.action = this.element.dataset.action.replace("click->button-component#openModal", "")
+    this.element.dataset.action = this.element.dataset.action.replace(`click->${this.identifier}#openModal`, "")
   }
 }
