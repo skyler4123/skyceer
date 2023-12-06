43b8fded19b2b75f457d1799431a67dc3fdaa58d
skyler <pkphung2001@gmail.com>
2023-12-06 21:01:39 +0700

    Update

--- app/components/button/up_to_top_component_controller.js
+++ app/components/button/up_to_top_component_controller.js
@@ -1,10 +1,22 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static values = {
+    klass: { type: String, default: "cursor-pointer" }
+  }
   connect() {
     // console.log("Hello, Stimulus!", this.element);
   }
   initialize() {
+    this.initializeKlass()
+    this.initializeAction()
+  }
+  initializeKlass() {
+    this.klassValue.split(' ').forEach((klass) => {
+      this.element.classList.add(klass)
+    })
+  }
+  initializeAction() {
     this.element.setAttribute('data-action', `click->${this.identifier}#upToTop`)
   }
   upToTop() {
