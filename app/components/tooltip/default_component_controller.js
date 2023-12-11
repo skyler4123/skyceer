47ad306eb15af056527aaa55dca2049b12a41b15
phong <pkphung2001@gmail.com>
2023-12-11 20:34:48 +0700

    Update

--- app/components/tooltip/default_component_controller.js
+++ app/components/tooltip/default_component_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ['arrow']
   static values = {
-    klass: { type: String, default: 'group-hover:flex group-hover:flex-col justify-center items-center hidden absolute z-10 cursor-default' },
+    klass: { type: String, default: 'group-hover:flex group-hover:flex-col justify-center items-center absolute z-10 cursor-default' },
     arrowClass: { type: String, default: 'absolute -z-10 top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rotate-45 w-3 h-3 bg-white border-t border-l border-gray-300' }
   }
   initialize() {
