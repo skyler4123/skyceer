983b259327887b7893645a42ccde1df07d5fb908
phong <pkphung2001@gmail.com>
2023-12-15 20:41:07 +0700

    Update

--- app/components/popover/default_component.rb
+++ app/components/popover/default_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Popover::DefaultComponent < ViewComponent::Base
-  def initialize(klass: 'bg-green-500 hidden w-96 h-fit', position: "bottom_center")
+  def initialize(klass: 'bg-green-500 hidden w-96 h-fit', position: nil)
     @klass = klass
     @position = position
   end
