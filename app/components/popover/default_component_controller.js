66a700b2d682be07b650e0f531bc20eb387c4111
phong <pkphung2001@gmail.com>
2023-12-16 10:36:59 +0700

    absolute

--- app/components/popover/default_component_controller.js
+++ app/components/popover/default_component_controller.js
@@ -14,40 +14,40 @@ export default class extends Controller {
   initializePosition() {
     switch(this.positionValue) {
       case 'bottom_left':
-        this.klassValue = 'justify-center items-center absolute -bottom-2 left-0 translate-y-full'
+        this.klassValue = 'justify-center items-center absolute z-10 -bottom-2 left-0 translate-y-full'
         break
       case 'bottom_center':
-        this.klassValue = 'justify-center items-center absolute -bottom-2 translate-y-full'
+        this.klassValue = 'justify-center items-center absolute z-10 -bottom-2 translate-y-full'
         break
       case 'bottom_right':
-        this.klassValue = 'justify-center items-center absolute -bottom-2 right-0 translate-y-full'
+        this.klassValue = 'justify-center items-center absolute z-10 -bottom-2 right-0 translate-y-full'
         break
       case 'top_right':
-        this.klassValue = 'justify-center items-center absolute -top-2 right-0 -translate-y-full'
+        this.klassValue = 'justify-center items-center absolute z-10 -top-2 right-0 -translate-y-full'
         break
       case 'top_left':
-        this.klassValue = 'justify-center items-center absolute -top-2 left-0 -translate-y-full'
+        this.klassValue = 'justify-center items-center absolute z-10 -top-2 left-0 -translate-y-full'
         break
       case 'top_center':
-        this.klassValue = 'justify-center items-center absolute -top-2 -translate-y-full'
+        this.klassValue = 'justify-center items-center absolute z-10 -top-2 -translate-y-full'
         break
       case 'left_top':
-        this.klassValue = 'justify-center items-center absolute top-0 -left-2 -translate-x-full'
+        this.klassValue = 'justify-center items-center absolute z-10 top-0 -left-2 -translate-x-full'
         break
       case 'left_bottom':
-        this.klassValue = 'justify-center items-center absolute bottom-0 -left-2 -translate-x-full'
+        this.klassValue = 'justify-center items-center absolute z-10 bottom-0 -left-2 -translate-x-full'
         break
       case 'left_center':
-        this.klassValue = 'justify-center items-center absolute -left-2 -translate-x-full'
+        this.klassValue = 'justify-center items-center absolute z-10 -left-2 -translate-x-full'
         break
       case 'right_top':
-        this.klassValue = 'justify-center items-center absolute top-0 -right-2 translate-x-full'
+        this.klassValue = 'justify-center items-center absolute z-10 top-0 -right-2 translate-x-full'
         break
       case 'right_bottom':
-        this.klassValue = 'justify-center items-center absolute bottom-0 -right-2 translate-x-full'
+        this.klassValue = 'justify-center items-center absolute z-10 bottom-0 -right-2 translate-x-full'
         break
       case 'right_center':
-        this.klassValue = 'justify-center items-center absolute -right-2 translate-x-full'
+        this.klassValue = 'justify-center items-center absolute z-10 -right-2 translate-x-full'
         break
     }
   }
