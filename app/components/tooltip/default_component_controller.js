f049c91bd3841782679631513cf589a8673538ca
phong <pkphung2001@gmail.com>
2023-12-16 10:36:21 +0700

    Tooltip z-10

--- app/components/tooltip/default_component_controller.js
+++ app/components/tooltip/default_component_controller.js
@@ -12,40 +12,40 @@ export default class extends Controller {
   initializePosition() {
     switch(this.positionValue) {
       case 'bottom_left':
-        this.klassValue = 'group-hover:flex absolute -bottom-2 left-0 translate-y-full'
+        this.klassValue = 'group-hover:flex absolute z-10 -bottom-2 left-0 translate-y-full'
         break
       case 'bottom_center':
-        this.klassValue = 'group-hover:flex absolute -bottom-2 translate-y-full'
+        this.klassValue = 'group-hover:flex absolute z-10 -bottom-2 translate-y-full'
         break
       case 'bottom_right':
-        this.klassValue = 'group-hover:flex absolute -bottom-2 right-0 translate-y-full'
+        this.klassValue = 'group-hover:flex absolute z-10 -bottom-2 right-0 translate-y-full'
         break
       case 'top_right':
-        this.klassValue = 'group-hover:flex absolute -top-2 right-0 -translate-y-full'
+        this.klassValue = 'group-hover:flex absolute z-10 -top-2 right-0 -translate-y-full'
         break
       case 'top_left':
-        this.klassValue = 'group-hover:flex absolute -top-2 left-0 -translate-y-full'
+        this.klassValue = 'group-hover:flex absolute z-10 -top-2 left-0 -translate-y-full'
         break
       case 'top_center':
-        this.klassValue = 'group-hover:flex absolute -top-2 -translate-y-full'
+        this.klassValue = 'group-hover:flex absolute z-10 -top-2 -translate-y-full'
         break
       case 'left_top':
-        this.klassValue = 'group-hover:flex absolute top-0 -left-2 -translate-x-full'
+        this.klassValue = 'group-hover:flex absolute z-10 top-0 -left-2 -translate-x-full'
         break
       case 'left_bottom':
-        this.klassValue = 'group-hover:flex absolute bottom-0 -left-2 -translate-x-full'
+        this.klassValue = 'group-hover:flex absolute z-10 bottom-0 -left-2 -translate-x-full'
         break
       case 'left_center':
-        this.klassValue = 'group-hover:flex absolute -left-2 -translate-x-full'
+        this.klassValue = 'group-hover:flex absolute z-10 -left-2 -translate-x-full'
         break
       case 'right_top':
-        this.klassValue = 'group-hover:flex absolute top-0 -right-2 translate-x-full'
+        this.klassValue = 'group-hover:flex absolute z-10 top-0 -right-2 translate-x-full'
         break
       case 'right_bottom':
-        this.klassValue = 'group-hover:flex absolute bottom-0 -right-2 translate-x-full'
+        this.klassValue = 'group-hover:flex absolute z-10 bottom-0 -right-2 translate-x-full'
         break
       case 'right_center':
-        this.klassValue = 'group-hover:flex absolute -right-2 translate-x-full'
+        this.klassValue = 'group-hover:flex absolute z-10 -right-2 translate-x-full'
         break
     }
   }
