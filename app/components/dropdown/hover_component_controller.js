0dc69d315281d74a4062f88bf5958f6a7dda2dc8
phong <pkphung2001@gmail.com>
2023-12-17 13:26:28 +0700

    Update

--- app/components/dropdown/hover_component_controller.js
+++ app/components/dropdown/hover_component_controller.js
@@ -6,7 +6,7 @@ export default class extends Controller {
     klass: { type: String, default: "relative inline-block group" },
     buttonClass: { type: String, default: "" },
     iconClass: { type: String, default: "rotate-0 open:rotate-180 duration-250" },
-    bodyClass: { type: String, default: "opacity-0 open:flex open:opacity-100 absolute z-10 transition-opacity duration-500 ease-in-out" }
+    bodyClass: { type: String, default: "h-0 opacity-0 open:h-fit open:flex open:opacity-100 absolute z-10 transition-opacity duration-500 ease-in-out" }
   }
 
   initialize() {
