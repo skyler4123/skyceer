95d48abe4bf684d74c50dc9e537a52f8d9ce4e19
phong <pkphung2001@gmail.com>
2023-12-17 21:13:37 +0700

    rework badge

--- app/components/badge/default_component.rb
+++ app/components/badge/default_component.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
-class Badge::DefaultComponent < ViewComponent::Base
-  renders_one :before
-  renders_one :after
-  
-  def initialize(label: "Badge", color: :blue)
+class Badge::DefaultComponent < ViewComponent::Base  
+  def initialize(klass: nil, label: "Badge Label", color: :blue, type: :default)
+    @klass = klass.to_s + ' hidden'
     @label = label
     @color = color.to_sym
+    @type = type.to_sym
   end
 end
