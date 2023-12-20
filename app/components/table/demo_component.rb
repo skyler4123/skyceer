c96bb441680d3407b0ba9f2569413abdcc067b0b
phong <pkphung2001@gmail.com>
2023-12-21 03:47:23 +0700

    Table::Demo

---
+++ app/components/table/demo_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Table::DemoComponent < ViewComponent::Base
+
+end
--- app/javascript/controllers/user_controller.js
+++ app/javascript/controllers/user_controller.js
@@ -1,7 +1,36 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static values = {
+    header: { type: Array, default: ['header 1', 'header 2', 'header 3'] },
+    body: { type: Array, default: [['1','2','3'], ['4','5','6'], ['7','8','9']] }
+  }
   connect() {
     // console.log('User Controller')
+  }
+  initialize() {
+    // console.log(this.table())
+  }
+  table() {
+    return `
+    <table class="bg-red-500">
+  <tr>
+    <th>${this.headerValue[0]}</th>
+    <th>${this.headerValue[1]}</th>
+    <th>${this.headerValue[2]}</th>
+  </tr>
+    ${this.bodyValue.map((row) => 
+      `
+      <tr>
+      <td>${row[0]}</td>
+      <td>${row[1]}</td>
+      <td>${row[2]}</td>
+    </tr>
+      `
+      ).join('')}
+      <div>wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww</div>
+</table>
+<div>wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww</div>
+    `
   }
 }
