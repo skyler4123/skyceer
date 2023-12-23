7bb046d52b5420d5db89f97f3e1233ec336bcad1
phong <pkphung2001@gmail.com>
2023-12-23 16:53:55 +0700

    Update

--- app/components/table/demo_component.rb
+++ app/components/table/demo_component.rb
@@ -6,7 +6,7 @@ class Table::DemoComponent < ViewComponent::Base
     header: ['header 1', 'header 2', 'header 3'] ,
     body: [['1','2','3'], ['4','5','6'], ['7','8','9']]
   )
-    @klass = klass
+    @klass = klass.to_s + ' hidden'
     @header = header
     @body = body
   end
