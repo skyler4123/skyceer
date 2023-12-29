48728ce939301246340818a4fe4376ced8e48746
phong <pkphung2001@gmail.com>
2023-12-29 20:36:11 +0700

    Update

--- app/components/dropdown/click_component_controller.js
+++ app/components/dropdown/click_component_controller.js
@@ -6,13 +6,14 @@ export default class extends Controller {
     klass: { type: String, default: "relative inline-block group" },
     buttonClass: { type: String, default: "" },
     iconClass: { type: String, default: "rotate-0 open:rotate-180 duration-250" },
-    bodyClass: { type: String, default: "h-0 opacity-0 open:h-fit open:flex open:opacity-100 absolute z-10 transition-opacity duration-500 ease-in-out" }
+    bodyClass: { type: String, default: "h-0 opacity-0 open:h-fit open:flex open:flex-col open:opacity-100 absolute z-10 transition-opacity duration-200 ease-in-out" }
   }
 
   initialize() {
-    // this.initializeTarget()
+    this.initializeTarget()
     // this.initializeHTML()
-    // this.initializeClass()
+    this.initializeClass()
+    this.initializeAction()
 
     this.initializeCompleted()
   }
@@ -20,15 +21,24 @@ export default class extends Controller {
     this.element.classList.remove('hidden')
   }
 
+  initializeTarget() {
+    this.headerTarget.querySelector("[data-controller='icon-component']").setAttribute(`data-${this.identifier}-target`, 'icon')
+  }
+
+  initializeClass() {
+    this.bodyTarget.className = this.bodyClassValue
+  }
+
+  initializeAction() {
+    this.headerTarget.setAttribute('data-action', `click->${this.identifier}#toggleDropdown`)
+  }
+
+  toggleDropdown() {
+    this.iconTarget.toggleAttribute('open')
+    this.bodyTarget.toggleAttribute('open')
+  }
 
   connect() {
     // console.log(this.identifier, this.element);
-  }
-  toggleDropdown() {
-    this.bodyTarget.toggleAttribute('open')
-    this.rotateIcon()
-  }
-  rotateIcon() {
-    this.iconTarget.toggleAttribute('open')
   }
 }
