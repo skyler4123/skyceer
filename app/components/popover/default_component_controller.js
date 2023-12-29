315218aa84794cdc6275740caec7b4fb60175c56
phong <pkphung2001@gmail.com>
2023-12-30 04:29:07 +0700

    Popover rework

--- app/components/popover/default_component_controller.js
+++ app/components/popover/default_component_controller.js
@@ -7,66 +7,33 @@ export default class extends Controller {
 
   initialize() {
     this.initializeParent()
-    this.initializeKlass()
-    this.initializeAttribute()
-    this.element.focus()
+    this.initializeClass()
   }
-  initializePosition() {
-    switch(this.positionValue) {
-      case 'bottom_left':
-        this.klassValue = 'justify-center items-center absolute z-10 -bottom-2 left-0 translate-y-full'
-        break
-      case 'bottom_center':
-        this.klassValue = 'justify-center items-center absolute z-10 -bottom-2 translate-y-full'
-        break
-      case 'bottom_right':
-        this.klassValue = 'justify-center items-center absolute z-10 -bottom-2 right-0 translate-y-full'
-        break
-      case 'top_right':
-        this.klassValue = 'justify-center items-center absolute z-10 -top-2 right-0 -translate-y-full'
-        break
-      case 'top_left':
-        this.klassValue = 'justify-center items-center absolute z-10 -top-2 left-0 -translate-y-full'
-        break
-      case 'top_center':
-        this.klassValue = 'justify-center items-center absolute z-10 -top-2 -translate-y-full'
-        break
-      case 'left_top':
-        this.klassValue = 'justify-center items-center absolute z-10 top-0 -left-2 -translate-x-full'
-        break
-      case 'left_bottom':
-        this.klassValue = 'justify-center items-center absolute z-10 bottom-0 -left-2 -translate-x-full'
-        break
-      case 'left_center':
-        this.klassValue = 'justify-center items-center absolute z-10 -left-2 -translate-x-full'
-        break
-      case 'right_top':
-        this.klassValue = 'justify-center items-center absolute z-10 top-0 -right-2 translate-x-full'
-        break
-      case 'right_bottom':
-        this.klassValue = 'justify-center items-center absolute z-10 bottom-0 -right-2 translate-x-full'
-        break
-      case 'right_center':
-        this.klassValue = 'justify-center items-center absolute z-10 -right-2 translate-x-full'
-        break
-    }
-  }
-  initializeKlass() {
-    this.initializePosition()
-    this.klassValue.split(' ').forEach((klass) => {
-      this.element.classList.add(klass)
-    })
+  initializeClass() {
+    this.element.className = this.positionClass()[this.positionValue]
   }
   initializeParent() {
-    this.parentController().classList.add('relative', 'group')
-    this.parentController().setAttribute('tabindex', 0)
-    this.parentController().setAttribute('data-action', `focusin->${this.parentController().dataset.controller}#openPopover focusout->${this.parentController().dataset.controller}#closePopover`)
-  }
-  initializeAttribute() {
-    this.element.setAttribute('tabindex', 0)
+    this.parentController().classList.add('relative')
+    this.parentController().setAttribute('data-action', `click->${this.parentController().dataset.controller}#togglePopover`)
   }
   parentController() {
     return this.element.parentNode.closest('[data-controller]')
+  }
+  positionClass() {
+    return {
+      'bottom_left': 'justify-center items-center absolute z-10 -bottom-2 left-0 translate-y-full',
+      'bottom_center': 'justify-center items-center absolute z-10 -bottom-2 translate-y-full',
+      'bottom_right': 'justify-center items-center absolute z-10 -bottom-2 right-0 translate-y-full',
+      'top_right': 'justify-center items-center absolute z-10 -top-2 right-0 -translate-y-full',
+      'top_left': 'justify-center items-center absolute z-10 -top-2 left-0 -translate-y-full',
+      'top_center': 'justify-center items-center absolute z-10 -top-2 -translate-y-full',
+      'left_top': 'justify-center items-center absolute z-10 top-0 -left-2 -translate-x-full',
+      'left_bottom': 'justify-center items-center absolute z-10 bottom-0 -left-2 -translate-x-full',
+      'left_center': 'justify-center items-center absolute z-10 -left-2 -translate-x-full',
+      'right_top': 'justify-center items-center absolute z-10 top-0 -right-2 translate-x-full',
+      'right_bottom': 'justify-center items-center absolute z-10 bottom-0 -right-2 translate-x-full',
+      'right_center': 'justify-center items-center absolute z-10 -right-2 translate-x-full'
+    }
   }
   connect() {
     // console.log("Hello, Stimulus!", this.element);
