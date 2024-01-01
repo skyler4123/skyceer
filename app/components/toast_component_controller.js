7eab73a3c7b8e0ff668a35a4bf4a1d992dc6689a
phong <pkphung2001@gmail.com>
2024-01-02 04:51:40 +0700

    Problem with close button

--- app/components/toast_component_controller.js
+++ app/components/toast_component_controller.js
@@ -4,9 +4,9 @@ export default class extends Controller {
   static targets = ["content"]
   static values = {
     isOpen: { type: Boolean, default: false },
-    position: { type: String, default: "bottom" },
-    klass: { type: String, default: " " },
-    contentClass: { type: String, default: " " },
+    position: { type: String, default: "left_center" },
+    klass: { type: String },
+    contentClass: { type: String },
     klassDefault: { type: String },
     contentClassDefault: { type: String },
   }
@@ -26,7 +26,23 @@ export default class extends Controller {
 
   initializeDefaultClass() {
     if (this.positionValue === "left") {
+      this.klassValue = this.klassValue || " "
+      this.contentClassValue = this.contentClassValue || " "
       this.klassDefaultValue = this.klassDefaultValue || " w-1/3 h-full bg-green-500 fixed top-0 left-0 z-20 open:translate-x-0 -translate-x-full duration-200 ease-in-out"
+      this.contentClassDefaultValue = this.contentClassDefaultValue || " "
+    }
+    if (this.positionValue === "left_top") {
+      this.klassDefaultValue = this.klassDefaultValue || " w-1/3 h-1/3 bg-green-500 fixed top-0 left-0 z-20 open:translate-x-0 -translate-x-full duration-200 ease-in-out"
+      this.contentClassDefaultValue = this.contentClassDefaultValue || " "
+    }
+    if (this.positionValue === "left_center") {
+      this.klassValue = this.klassValue || " w-1/3 h-1/3 bg-green-500 open:translate-x-0"
+      this.contentClassValue = this.contentClassValue || " "
+      this.klassDefaultValue = this.klassDefaultValue || " fixed top-1/2 left-0 z-20 -translate-x-full -translate-y-1/2 duration-200 ease-in-out"
+      this.contentClassDefaultValue = this.contentClassDefaultValue || " "
+    }
+    if (this.positionValue === "left_bottom") {
+      this.klassDefaultValue = this.klassDefaultValue || " w-1/3 h-1/3 bg-green-500 fixed bottom-0 left-0 z-20 open:translate-x-0 -translate-x-full duration-200 ease-in-out"
       this.contentClassDefaultValue = this.contentClassDefaultValue || " "
     }
     if (this.positionValue === "right") {
