afe702d53dcbb0b0b69b9810d9e2999c63fd20c3
phong <pkphung2001@gmail.com>
2024-01-06 03:49:50 +0700

    Update

--- app/components/close_component.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class CloseComponent < ViewComponent::Base
-  def initialize(klass: nil, target_id: nil, target_controller_name: nil)
-    @klass = klass
-    @target_id = target_id
-    @target_controller_name = target_controller_name
-  end
-end
