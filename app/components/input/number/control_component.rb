8114a67115943d45e74d10494f10795c8cdbac7f
phong <pkphung2001@gmail.com>
2024-01-07 17:30:24 +0700

    Add components

--- app/components/input/number/control_component.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Input::Number::ControlComponent < ViewComponent::Base
-
-end
--- app/components/input/pin_code_component.rb
+++ app/components/input/pin_code_component.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Input::Number::PinCodeComponent < ViewComponent::Base
+class Input::PinCodeComponent < ViewComponent::Base
   def initialize(klass: nil)
     @klass = 'hidden ' + klass.to_s
   end
