e6a4551b74853977754016795a8fa5765ded4dd6
phong <pkphung2001@gmail.com>
2024-01-11 21:52:56 +0700

    Update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
     "My name is Skyler! And Im doing StimulusJS!!!" +
     render(IconComponent.new(name: 'sun', klass: "inline-block", svg_class: "w-4 h-4 text-yellow-500 animate-ping"))
   end
+
+  def demo_button
+    render(ButtonComponent.new(label: 1, id: 1, class: 1, event: { id: 1, action: :toggle, listener: :click }))
+  end
 end
