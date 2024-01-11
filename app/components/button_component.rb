2fa087fb0704375382bf4d685c59f50f70026857
phong <pkphung2001@gmail.com>
2024-01-11 21:12:48 +0700

    Update

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -1,11 +1,34 @@
 # frozen_string_literal: true
+# typed: true
 
 class ButtonComponent < ViewComponent::Base
+  extend T::Sig
+
   renders_many :prepends
   renders_many :appends
   renders_one :body
   renders_many :hiddens
   
+  sig {params(
+    controller: T.nilable(String),
+    controller_action: T.nilable(String),
+    event: T.nilable(String),
+    id: T.nilable(String),
+
+    label: T.nilable(String),
+    src: T.nilable(String),
+    url: T.nilable(String),
+    
+    klass: T.nilable(String),
+    content_class: T.nilable(String),
+    button_class: T.nilable(String),
+    link_class: T.nilable(String),
+    klass_default: T.nilable(String),
+    content_class_default: T.nilable(String),
+    button_class_default: T.nilable(String),
+    link_class_default: T.nilable(String),
+    rest_data_attributes: T.nilable(Hash)
+    ).void}
   def initialize(
     controller: nil,
     controller_action: nil,
