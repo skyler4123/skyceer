9edc14945d093a0a325516b52726c7ff9b90adbf
phong <pkphung2001@gmail.com>
2024-01-20 17:04:35 +0700

    Rework Img

--- app/components/img_component.rb
+++ app/components/img_component.rb
@@ -1,40 +1,9 @@
 # frozen_string_literal: true
 
 class ImgComponent < ViewComponent::Base
-  def initialize(
-    controller: nil,
-    controller_action: nil,
-    is_open: nil,
-    event: nil,
-    id: nil,
-
-    src: nil,
-    url: nil,
-    alt: nil,
-
-    klass: nil,
-    img_class: nil,
-    klass_default: nil,
-    img_class_default: nil,
-    **rest_data_attributes
-  )
+  def initialize( controller: nil, **options)
     @controller = "img-component" + ' ' + controller.to_s
-    @controller_action = controller_action
-    @is_open = is_open
-    @event = event
-    @id = id
-
-    @url = url || src
-    @alt = alt
-
-    @klass = klass
-    @img_class = img_class
-    @klass_default = klass_default
-    @img_class_default = img_class_default
-    @rest_data_attributes = rest_data_attributes
-    
-    if @event && @event[:action]
-      @event[:action] = @event[:action].to_s.camelize(:lower) 
-    end
+    options.keys.each {|key| options[key.to_s.camelize(:lower).to_sym] = options.delete(key)}
+    @options = options.to_json
   end
 end
--- app/components/img_component_controller.js
+++ app/components/img_component_controller.js
@@ -1,33 +1,25 @@
-import morphdom from "morphdom"
+import { twMerge } from 'tailwind-merge'
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["template", "img"]
+  static targets = ['img']
   static values = {
-    isOpen: { type: Boolean, default: true },
-    event: { type: Object },
-    canSendGlobalDispatch: { type: Boolean, default: false },
-    canReceiveGlobalDispatch: { type: Boolean, default: false },
-
-    type: { type: String },
-    url: { type: String, default: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png" },
-    alt: { type: String, default: "image description" },
-
-    klass: { type: String },
-    imgClass: { type: String, default: "h-24 w-24 object-center transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0" },
-    klassDefault: { type: String },
-    imgClassDefault: { type: String }
+    options: { type: Object },
+    isOpen: { type: Boolean },
+    isFocus: { type: Boolean },
+    isActive: { type: Boolean },
   }
 
   initialize() {
     this.initializeID()
     this.initializeHTML()
-    this.initializeTarget()
-    this.initializeDefaultClass()
     this.initializeClass()
     this.initializeAction()
-    
+
     this.initializeComplete()
+  }
+  connect() {
+    if (this.isTest) { console.log(this) }
   }
   initializeID() {
     if (!this.element.id) {
@@ -36,99 +28,78 @@ export default class extends Controller {
   }
   initializeComplete() {
     this.element.classList.remove('hidden')
-    this.dispatch('dispatch', { detail: { payload: { id: this.element.id, action: "complete", controller: this } } })
+  }
+  get klass() {
+    return this.optionsValue.klass
+  }
+  get imgClass() {
+    return this.optionsValue.imgClass
+  }
+  get id() {
+    return this.element.id
+  }
+  get isTest() {
+    return this.optionsValue.test
+  }
+  get event() {
+    return this.optionsValue.event
+  }
+  get eventId() {
+    return this.event.id
+  }
+  get url() {
+    return this.optionsValue.url
+  }
+  get alt() {
+    return this.optionsValue.alt
   }
 
   initializeHTML() {
-    morphdom(this.templateTarget, this.initHTML())
-  }
-
-  initializeTarget() {
-    this.element.querySelector('img').setAttribute(`data-${this.identifier}-target`, 'img')
-  }
-
-  initializeDefaultClass() {
-    if (this.typeValue === "circle") {
-      this.imgClassDefaultValue = "rounded-full"
-    }
+    this.element.innerHTML = this.initHTML()
   }
 
   initializeClass() {
-    this.element.className = this.element.className + ' ' + this.klassDefaultValue + ' ' + this.klassValue
-    this.imgTarget.className = this.imgTarget.className + ' ' + this.imgClassDefaultValue + ' ' + this.imgClassValue
+    this.element.className = twMerge(this.klass)
+    this.imgTarget.className = twMerge(this.imgClass)
   }
-  
-  initHTML() {
-    return `
-      <img src="${this.urlValue}" alt="${this.altValue}">
-    `
-  }
-  
+
   initializeAction() {
-    if (this.eventValue?.id && this.eventValue?.listener && this.eventValue?.action) {
-      this.canSendGlobalDispatchValue = true
-    }
-    if (this.eventValue?.id && !this.eventValue?.listener && !this.eventValue?.action) {
-      this.canReceiveGlobalDispatchValue = true
-    }
-  }
-
-  canSendGlobalDispatchValueChanged(value, previousValue) {
-    if (this.canSendGlobalDispatchValue) {
-      if (this.eventValue.listener === 'click') {
-        this.element.dataset.action = (this.element.dataset.action || '') + ' ' + `click->${this.identifier}#${this.eventValue.action}`
-      }
-      if (this.eventValue.listener === 'hover') {
-        this.element.dataset.action = (this.element.dataset.action || '') + ' ' + `mouseenter->${this.identifier}#${this.eventValue.action} mouseleave->${this.identifier}#${this.eventValue.action}`
-      }
-    }
-  }
-
-  canReceiveGlobalDispatchValueChanged() {
-    if (this.canReceiveGlobalDispatchValue) {
-      this.element.dataset.action = (this.element.dataset.action || "") + ` global:dispatch@window->${this.identifier}#globalDispatch`
-    }
+    this.element.dataset.action = (this.element.dataset.action || "") + ` global:dispatch@window->${this.identifier}#globalDispatch`
   }
 
   globalDispatch({ detail: { event } }) {
-    if (this.eventValue.id === event.id && this.element.id !== event.controller.element.id) {
+    if (this.eventId === event.id && this.id !== event.controller.id) {
       eval(`this.${event.action}(event)`)
     }
   }
 
-  toggle(event) {
+  toggle() {
     this.isOpenValue = !this.isOpenValue
-    if (this.canSendGlobalDispatchValue) {
-      this.dispatch('dispatch', { detail: { event: { ...this.eventValue, controller: this } } })
-      event.stopPropagation()
-    }
   }
 
-  open(event) {
+  open() {
     this.isOpenValue = true
-    if (this.canSendGlobalDispatchValue) {
-      this.dispatch('dispatch', { detail: { event: { ...this.eventValue, controller: this } } })
-      event.stopPropagation()
-    }
   }
 
-  close(event) {
+  close() {
     this.isOpenValue = false
-    if (this.canSendGlobalDispatchValue) {
-      this.dispatch('dispatch', { detail: { event: { ...this.eventValue, controller: this } } })
-      event.stopPropagation()
-    }
   }
 
   isOpenValueChanged(value, previousValue) {
     if (this.isOpenValue) {
       this.element.setAttribute('open', '')
+      this.imgTarget.setAttribute('open', '')
     } else {
       this.element.removeAttribute('open')
+      this.imgTarget.removeAttribute('open')
     }
   }
-
-  connect() {
-    // console.log("Hello, Stimulus!", this.element);
+  rotate() {
+    this.isOpenValue = !this.isOpenValue
+  }
+  initHTML() {
+    return `
+      <img data-${this.identifier}-target="img" src="${this.url}" alt="${this.alt}">
+    `
   }
 }
