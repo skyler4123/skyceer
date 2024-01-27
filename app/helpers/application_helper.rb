bfc61a6d921a25e79f827f674d9ff111d11ecad2
phong <pkphung2001@gmail.com>
2024-01-27 13:08:28 +0700

    Update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
   include Pagy::Frontend
 
-  def button(**kwarg)
-    render(ButtonComponent.new(label: "Test Button", **kwarg))
-  end
-
   Dir.glob('app/components/*.rb').map {|dir| dir.split('/').last.split('.').first}.each do |component_name|
     define_method(component_name.split('_').first) do |**kwargs, &block|
       component = component_name.to_s.camelize.constantize
