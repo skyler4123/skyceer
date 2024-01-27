087f54945b866d371f0f801bd0a44a7810d8074e
phong <pkphung2001@gmail.com>
2024-01-27 13:03:15 +0700

    Add define_method for components

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
   def button(**kwarg)
     render(ButtonComponent.new(label: "Test Button", **kwarg))
   end
+
+  Dir.glob('app/components/*.rb').map {|dir| dir.split('/').last.split('.').first}.each do |component_name|
+    define_method(component_name.split('_').first) do |**kwargs, &block|
+      component = component_name.to_s.camelize.constantize
+      render(component.new(**kwargs), &block)
+    end
+  end
 end
