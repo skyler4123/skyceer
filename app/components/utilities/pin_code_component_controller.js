e1e6d4cb6887470a7ab2f20294b40a974a2a97ad
phong <pkphung2001@gmail.com>
2024-02-21 20:15:53 +0700

    Done PinCode

--- app/components/utilities/pin_code_component_controller.js
+++ app/components/utilities/pin_code_component_controller.js
@@ -5,7 +5,6 @@ export default class extends ApplicationComponentController {
   static values = {
     ...super.values,
     index: { type: Number, default: 0 },
-    name: { type: String, default: 'default_name' },
     input: { type: String }
   }
 
@@ -24,6 +23,15 @@ export default class extends ApplicationComponentController {
 
   initializeClass() {
     this.element.className = this.twMerge(this.element.className, this.klass)
+  }
+
+  keyup(event) {
+    if (event.code === 'Backspace') {
+      this.focusPrevious()
+    } else {
+      this.focusNext()
+    }
+    this.inputValue = this.inputString
   }
 
   focus(event) {
@@ -45,29 +53,32 @@ export default class extends ApplicationComponentController {
     if (previousValue === undefined || previousValue === '') { return }
 
     this.cellTargets[this.indexValue].focus()
-    this.inputValue = this.inputString
+  }
+
+  inputValueChanged() {
+    this.inputTarget.value = this.inputValue
   }
 
   get initHTML() {
     return `
       <input class='hidden' name="${this.nameValue}" type="text" data-${this.identifier}-target="input">
       <div>
-        <input data-${this.identifier}-target="cell" data-action="keyup->${this.identifier}#focusNext click->${this.identifier}#focus" type="text" maxlength="1" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
+        <input data-${this.identifier}-target="cell" data-action="keyup->${this.identifier}#keyup click->${this.identifier}#focus" type="text" maxlength="1" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
       </div>
       <div>
-        <input data-${this.identifier}-target="cell" data-action="keyup->${this.identifier}#focusNext click->${this.identifier}#focus" type="text" maxlength="1" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
+        <input data-${this.identifier}-target="cell" data-action="keyup->${this.identifier}#keyup click->${this.identifier}#focus" type="text" maxlength="1" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
       </div>
       <div>
-        <input data-${this.identifier}-target="cell" data-action="keyup->${this.identifier}#focusNext click->${this.identifier}#focus" type="text" maxlength="1" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
+        <input data-${this.identifier}-target="cell" data-action="keyup->${this.identifier}#keyup click->${this.identifier}#focus" type="text" maxlength="1" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
       </div>
       <div>
-        <input data-${this.identifier}-target="cell" data-action="keyup->${this.identifier}#focusNext click->${this.identifier}#focus" type="text" maxlength="1" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
+        <input data-${this.identifier}-target="cell" data-action="keyup->${this.identifier}#keyup click->${this.identifier}#focus" type="text" maxlength="1" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
       </div>
       <div>
-        <input data-${this.identifier}-target="cell" data-action="keyup->${this.identifier}#focusNext click->${this.identifier}#focus" type="text" maxlength="1" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
+        <input data-${this.identifier}-target="cell" data-action="keyup->${this.identifier}#keyup click->${this.identifier}#focus" type="text" maxlength="1" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
       </div>
       <div>
-        <input data-${this.identifier}-target="cell" data-action="keyup->${this.identifier}#focusNext click->${this.identifier}#focus" type="text" maxlength="1" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
+        <input data-${this.identifier}-target="cell" data-action="keyup->${this.identifier}#keyup click->${this.identifier}#focus" type="text" maxlength="1" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
       </div>
     `
   }
@@ -82,5 +93,8 @@ export default class extends ApplicationComponentController {
   }
   get currentCell() {
     return this.cellTargets[this.indexValue]
+  }
+  get name() {
+    return this.optionsValue.name
   }
 }
