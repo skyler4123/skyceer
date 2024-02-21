e4a67dc497a113b6305e12e12828030b4036aa99
phong <pkphung2001@gmail.com>
2024-02-22 05:18:39 +0700

    Init Label

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -3,7 +3,8 @@
 class ApplicationComponent < ViewComponent::Base
   include ApplicationHelper
   
-  def initialize( controller: nil, **options)
+  def initialize( controller: nil, tag: :div, **options)
+    @tag = tag
     @component_name = self.class.name.underscore.dasherize.gsub('/', '--')
     @controller = @component_name + ' ' + controller.to_s
     @options = options.to_json
