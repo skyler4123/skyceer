3a0e9f0e12263fcc66fcd0f7053da1b258bf1ffa
phong <pkphung2001@gmail.com>
2024-03-05 05:23:29 +0700

    Update

---
+++ app/javascript/static/layouts/application_controller.js
@@ -0,0 +1,17 @@
+
+import ApplicationController from '../../controllers/application_controller'
+
+export default class extends ApplicationController {
+
+  initialize() {
+
+  }
+
+  get headerHTML() {
+
+  }
+
+  get footerHTML() {
+    
+  }
+}
