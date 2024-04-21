c1d24fcbbe8db32b451e6e6a5133a2df668ec37a
skyler <pkphung2001@gmail.com>
2024-04-21 12:10:29 +0700

    u

---
+++ algorithm/array/sorting/quick.rb
@@ -0,0 +1,21 @@
+def quick(array = [9,0,8,2,1,3,5,4,7,6])
+  length = array.length
+  return array if length < 2
+  pivot = array.last
+  smaller_array = []
+  bigger_array = []
+  (length - 1).times do |i|
+    if array[i] < pivot
+      smaller_array << array[i]
+    end
+    if array[i] > pivot
+      bigger_array << array[i]
+    end
+  end
+  smaller_array = quick(smaller_array)
+  bigger_array = quick(bigger_array)
+  result = smaller_array.flatten + [pivot] + bigger_array.flatten 
+  return result
+end
+
+puts quick.to_s
