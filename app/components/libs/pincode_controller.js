04bc29b5547f0a1af1a9b75a132b2ed9bcd15843
skyler <pkphung2001@gmail.com>
2024-04-24 19:05:40 +0700

    u

--- app/components/libs/pincode_controller.js
+++ app/components/libs/pincode_controller.js
@@ -61,7 +61,7 @@ export default class Pincode extends ApplicationController {
 
   get typeHTML() {
     return `
-      <input class='hidden flex flex-row gap-x-3' name="${this.nameParams}" type="text" data-${this.identifier}-target="input">
+      <input class='hidden' name="${this.nameParams}" type="text" data-${this.identifier}-target="input">
       <div>
         <input data-${this.identifier}-target="cell" data-action="keyup->${this.identifier}#keyup click->${this.identifier}#focus" type="text" maxlength="1" class="block w-9 h-9 py-3 text-sm font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
       </div>
