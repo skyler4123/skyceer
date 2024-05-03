2a0d0dc59579d6b972db5c492f58a4240ac01eab
skyler <pkphung2001@gmail.com>
2024-05-04 06:54:05 +0700

    add demos model

---
+++ db/migrate/20240503235318_create_demos.rb
@@ -0,0 +1,18 @@
+class CreateDemos < ActiveRecord::Migration[7.1]
+  def change
+    create_table :demos, id: :uuid do |t|
+      t.string :string
+      t.text :text
+      t.float :float
+      t.decimal :decimal
+      t.datetime :datetime
+      t.time :time
+      t.date :date
+      t.binary :binary
+      t.boolean :boolean
+      t.json :json
+
+      t.timestamps
+    end
+  end
+end
