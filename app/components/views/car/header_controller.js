58beb6879374f893ee9b68fdcd01749cf0ef9342
skyler <pkphung2001@gmail.com>
2024-05-21 07:05:59 +0700

    update

--- app/components/views/car/header_controller.js
+++ app/components/views/car/header_controller.js
@@ -1,7 +1,14 @@
 import ApplicationController from "../../../javascript/controllers/application_controller";
+import { button } from "../../../javascript/controllers/components";
 
 export default class extends ApplicationController {
   connect() {
     // console.log("Hello, Stimulus!", this.element);
+  }
+  init() {
+    this.element.innerHTML = `
+      <div>Headerrrrrrrrrrrrrrrrrrrrrrrrrrrr</div>
+      ${button({label: 'Button'})}
+    `
   }
 }
