21495e8d361c02e5c8be5c4a6863438477abd983
skyler <pkphung2001@gmail.com>
2024-06-25 07:13:45 +0700

    update

--- algorithm/facebook/script.js
+++ algorithm/facebook/script.js
@@ -21,7 +21,7 @@ document.querySelectorAll(`[class="${seeMoreClass}"]`).forEach(target => {
 
 // Search number of comments
 let numberFrom = 0
-let numberTo = 99
+let numberTo = 999
 let position = 1
 
 let commentGroupClass = "html-div xe8uvvx x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1gslohp"
