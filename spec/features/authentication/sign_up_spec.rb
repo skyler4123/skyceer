d6979459e213effd7e126dc39ec697e1fea217c3
skyler <pkphung2001@gmail.com>
2024-07-11 07:37:44 +0700

    update

--- spec/features/authentication/sign_up_spec.rb
+++ spec/features/authentication/sign_up_spec.rb
@@ -7,18 +7,19 @@ RSpec.feature "Authentication::SignUps", type: :feature, js: true do
     Capybara.use_default_driver
     # default_url_options[:host] = 'localhost:3000'
     Capybara.app_host = "http://" + 'localhost'
-    # Capybara.server_port = '1234'
+    Capybara.server_port = '1234'
   end
 
   it "signs me in" do
     # debugger
     visit '/sign_up'
-    sleep 15
     # within("#session") do
     #   fill_in 'Email', with: 'user@example.com'
     #   fill_in 'Password', with: 'password'
     # end
     # click_button 'Sign in'
     expect(true).to be(true)
+    sleep 15
+
   end
 end
