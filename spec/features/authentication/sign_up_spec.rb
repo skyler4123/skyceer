1fa5662c01b099a55b9c92db9ab2edd0fd115641
skyler <pkphung2001@gmail.com>
2024-07-12 06:53:40 +0700

    update

--- spec/features/authentication/sign_up_spec.rb
+++ spec/features/authentication/sign_up_spec.rb
@@ -1,25 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "Authentication::SignUps", type: :feature, js: true do
-  before :each do
-    # User.create(email: 'user@example.com', password: 'password')
-    Capybara.default_driver = :selenium_chrome
-    Capybara.use_default_driver
-    # default_url_options[:host] = 'localhost:3000'
-    Capybara.app_host = "http://" + 'localhost'
-    Capybara.server_port = '1234'
-  end
 
   it "signs me in" do
-    # debugger
     visit '/sign_up'
     # within("#session") do
-    #   fill_in 'Email', with: 'user@example.com'
-    #   fill_in 'Password', with: 'password'
+    fill_in 'email', with: 'user@example.com'
+    fill_in 'password', with: 'password'
+    fill_in 'password_confirmation', with: 'password'
+    # click_on "Sign Up"
+
     # end
     # click_button 'Sign in'
-    expect(true).to be(true)
-    sleep 15
+    # expect(true).to be(true)
+    sleep 60
 
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -76,3 +76,8 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+Capybara.javascript_driver = :selenium_chrome
