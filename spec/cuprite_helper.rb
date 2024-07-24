583c46e6d3720be4f0203cb1f59fdd3869bec1e1
skyler <pkphung2001@gmail.com>
2024-07-25 04:06:49 +0700

    update

--- spec/cuprite_helper.rb
+++ spec/cuprite_helper.rb
@@ -1,6 +1,9 @@
 require "capybara/cuprite"
 
 Capybara.javascript_driver = :cuprite
+browser_options = {
+  'no-sandbox': nil
+}
 options = {
   headless: true, # (String | Boolean) - Set browser as headless or not, true by default. You can set "new" to support new headless mode.
   # xvfb: false, # (Boolean) - Run browser in a virtual framebuffer, false by default.
@@ -14,7 +17,7 @@ options = {
   # pending_connection_errors: true, # (Boolean) - When main frame is still waiting for slow responses while timeout is reached PendingConnectionsError is raised. It's better to figure out why you have slow responses and fix or block them rather than turn this setting off. Default is true.
   # browser_name: :chrome, # (Symbol) - :chrome by default, only experimental support for :firefox for now.
   # browser_path: nil, # (String) - Path to Chrome binary, you can also set ENV variable as BROWSER_PATH=some/path/chrome bundle exec rspec.
-  # browser_options: {}, # (Hash) - Additional command line options, see them all e.g. { "ignore-certificate-errors" => nil }
+  browser_options: browser_options, # (Hash) - Additional command line options, see them all e.g. { "ignore-certificate-errors" => nil }
   # ignore_default_browser_options: nil, # (Boolean) - Ferrum has a number of default options it passes to the browser, if you set this to true then only options you put in :browser_options will be passed to the browser, except required ones of course.
   # port: nil, # (Integer) - Remote debugging port for headless Chrome.
   # host: nil, # (String) - Remote debugging address for headless Chrome.
