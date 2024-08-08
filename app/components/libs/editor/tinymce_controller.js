ffe0ba13ef72471b18b82daf79ea2dcb4c7c21da
skyler <pkphung2001@gmail.com>
2024-08-09 05:20:34 +0700

    update

--- app/components/libs/editor/tinymce_controller.js
+++ app/components/libs/editor/tinymce_controller.js
@@ -8,6 +8,11 @@ export default class extends ApplicationController {
     console.log("Hello, Stimulus!", tinymce);
     tinymce.init({
       selector: '#editor',
+      plugins: 'advlist lists code',
+      menubar: false,
+      toolbar: 'bullist numlist code',
+      advlist_bullet_styles: 'square',
+      advlist_number_styles: 'lower-alpha,lower-roman,upper-alpha,upper-roman'
     });
   }
 }
