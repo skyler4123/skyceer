fd29449350c249df1f8e1fe7708146c2c757ae9d
skyler <pkphung2001@gmail.com>
2024-08-09 20:10:43 +0700

    update

--- app/components/libs/editor/tinymce_component.rb
+++ app/components/libs/editor/tinymce_component.rb
@@ -3,9 +3,14 @@
 class Libs::Editor::TinymceComponent < ApplicationComponent
 
   erb_template <<-ERB
-    <script src="#{TINYMCE_URL}" referrerpolicy="origin"></script>
+    <script src="<%= @tinymce_url %>" referrerpolicy="origin"></script>
     <<%= @tag %> class="hidden" data-controller="<%= @controllers %>" data-<%= @controller_name %>-params-value="<%= @params %>">
       <div data-<%= @controller_name %>-target="editor" id="editor"></div>
     </<%= @tag %>>
   ERB
+
+  def initialize(controller: nil, tag: :div, data: nil, tinymce_url: TINYMCE_URL, **params)
+    @tinymce_url = tinymce_url
+    super
+  end
 end
