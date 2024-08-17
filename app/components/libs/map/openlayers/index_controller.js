7f97fcb80acf8c10fc46239ada289d5ab69ef002
skyler <pkphung2001@gmail.com>
2024-08-18 00:19:59 +0700

    update

--- app/components/libs/map/openlayers/index_controller.js
+++ app/components/libs/map/openlayers/index_controller.js
@@ -1,54 +1,42 @@
 import openlayers from "openlayers"
 import OpenlayersController from "../openlayers_controller";
 
-
-const Map = openlayers.Map
-const View = openlayers.View
-const Feature = openlayers.Feature
-const Overlay = openlayers.Overlay
-
-const TiltLayer = openlayers.layer.Tile
-const VectorLayer = openlayers.layer.Vector
-
-const OSM = openlayers.source.OSM
-const VectorSource = openlayers.source.Vector
-const Stamen = openlayers.source.Stamen
-const XYZ = openlayers.source.XYZ
-
-const GeoJSON = openlayers.format.GeoJSON
-const KML = openlayers.format.KML
-
-const DragAndDrop = openlayers.interaction.DragAndDrop
-const Modify = openlayers.interaction.Modify
-const Draw = openlayers.interaction.Draw
-const Snap = openlayers.interaction.Snap
-const Select = openlayers.interaction.Select
-
-const Style = openlayers.style.Style
-const Stroke = openlayers.style.Stroke
-const Fill = openlayers.style.Fill
-const Text = openlayers.style.Text
-const CircleStyle = openlayers.style.Circle
-const Icon = openlayers.style.Icon
-
-const getArea = openlayers.Sphere.getArea
-
-const fromExtent = openlayers.geom.Polygon.fromExtent
-const Point = openlayers.geom.Point
-const MultiPoint = openlayers.geom.MultiPoint
-const circular = openlayers.geom.Polygon.circular
-
-const VectorContext = openlayers.render.VectorContext
-
-const toLonLat = openlayers.proj.toLonLat
-const fromLonLat = openlayers.proj.fromLonLat
-
-const toStringHDMS = openlayers.coordinate.toStringHDMS
-
-const click = openlayers.events.condition.click
-const pointerMove = openlayers.events.condition.pointerMove
-
 export default class extends OpenlayersController {
+  get Map() { return openlayers.Map }
+  get View() { return openlayers.View }
+  get Feature() { return openlayers.Feature }
+  get Overlay() { return openlayers.Overlay }
+  get TiltLayer() { return openlayers.layer.Tile }
+  get VectorLayer() { return openlayers.layer.Vector }
+  get OSM() { return openlayers.source.OSM }
+  get VectorSource() { return openlayers.source.Vector }
+  get Stamen() { return openlayers.source.Stamen }
+  get XYZ() { return openlayers.source.XYZ }
+  get GeoJSON() { return openlayers.format.GeoJSON }
+  get KML() { return openlayers.format.KML }
+  get DragAndDrop() { return openlayers.interaction.DragAndDrop }
+  get Modify() { return openlayers.interaction.Modify }
+  get Draw() { return openlayers.interaction.Draw }
+  get Snap() { return openlayers.interaction.Snap }
+  get Select() { return openlayers.interaction.Select }
+  get Style() { return openlayers.style.Style }
+  get Stroke() { return openlayers.style.Stroke }
+  get Fill() { return openlayers.style.Fill }
+  get Text() { return openlayers.style.Text }
+  get CircleStyle() { return openlayers.style.Circle }
+  get Icon() { return openlayers.style.Icon }
+  get getArea() { return openlayers.Sphere.getArea }
+  get fromExtent() { return openlayers.geom.Polygon.fromExtent }
+  get Point() { return openlayers.geom.Point }
+  get MultiPoint() { return openlayers.geom.MultiPoint }
+  get circular() { return openlayers.geom.Polygon.circular }
+  get VectorContext() { return openlayers.render.VectorContext }
+  get toLonLat() { return openlayers.proj.toLonLat }
+  get fromLonLat() { return openlayers.proj.fromLonLat }
+  get toStringHDMS() { return openlayers.coordinate.toStringHDMS }
+  get click() { return openlayers.events.condition.click }
+  get pointerMove() { return openlayers.events.condition.pointerMove }
+
   static targets = ['map']
   static values = {
     pointsCoordinates: { type: Array, default: [] }
@@ -68,11 +56,11 @@ export default class extends OpenlayersController {
   } // initComplete
 
   initOpenlayersIndex() {
-    this.pointsSource = new VectorSource({})
-    this.pointsLayer = new VectorLayer({
+    this.pointsSource = new this.VectorSource({})
+    this.pointsLayer = new this.VectorLayer({
       source: this.pointsSource,
-      style: new Style({
-        image: new Icon({
+      style: new this.Style({
+        image: new this.Icon({
           anchor: [0.5, 850],
           anchorXUnits: 'fraction',
           anchorYUnits: 'pixels',
@@ -98,9 +86,9 @@ export default class extends OpenlayersController {
   }
 
   pointFeature(point) {
-    let coordinates = fromLonLat([point.longitude, point.latitude])
-    return new Feature({
-      geometry: new Point(coordinates),
+    let coordinates = this.fromLonLat([point.longitude, point.latitude])
+    return new this.Feature({
+      geometry: new this.Point(coordinates),
       ...point,
     })
   }
--- app/components/libs/map/openlayers/show_controller.js
+++ app/components/libs/map/openlayers/show_controller.js
@@ -1,53 +1,6 @@
 import openlayers from "openlayers"
 import OpenlayersController from "../openlayers_controller";
 
-
-const Map = openlayers.Map
-const View = openlayers.View
-const Feature = openlayers.Feature
-const Overlay = openlayers.Overlay
-
-const TiltLayer = openlayers.layer.Tile
-const VectorLayer = openlayers.layer.Vector
-
-const OSM = openlayers.source.OSM
-const VectorSource = openlayers.source.Vector
-const Stamen = openlayers.source.Stamen
-const XYZ = openlayers.source.XYZ
-
-const GeoJSON = openlayers.format.GeoJSON
-const KML = openlayers.format.KML
-
-const DragAndDrop = openlayers.interaction.DragAndDrop
-const Modify = openlayers.interaction.Modify
-const Draw = openlayers.interaction.Draw
-const Snap = openlayers.interaction.Snap
-const Select = openlayers.interaction.Select
-
-const Style = openlayers.style.Style
-const Stroke = openlayers.style.Stroke
-const Fill = openlayers.style.Fill
-const Text = openlayers.style.Text
-const CircleStyle = openlayers.style.Circle
-const Icon = openlayers.style.Icon
-
-const getArea = openlayers.Sphere.getArea
-
-const fromExtent = openlayers.geom.Polygon.fromExtent
-const Point = openlayers.geom.Point
-const MultiPoint = openlayers.geom.MultiPoint
-const circular = openlayers.geom.Polygon.circular
-
-const VectorContext = openlayers.render.VectorContext
-
-const toLonLat = openlayers.proj.toLonLat
-const fromLonLat = openlayers.proj.fromLonLat
-
-const toStringHDMS = openlayers.coordinate.toStringHDMS
-
-const click = openlayers.events.condition.click
-const pointerMove = openlayers.events.condition.pointerMove
-
 export default class extends OpenlayersController {
   static targets = ['map']
   static values = {
@@ -68,11 +21,11 @@ export default class extends OpenlayersController {
   } // initComplete
 
   initOpenlayersShow() {
-    this.pointSource = new VectorSource({})
-    this.pointLayer = new VectorLayer({
+    this.pointSource = new this.VectorSource({})
+    this.pointLayer = new this.VectorLayer({
       source: this.pointSource,
-      style: new Style({
-        image: new Icon({
+      style: new this.Style({
+        image: new this.Icon({
           anchor: [0.5, 850],
           anchorXUnits: 'fraction',
           anchorYUnits: 'pixels',
@@ -92,9 +45,9 @@ export default class extends OpenlayersController {
   }
 
   pointFeature() {
-    let coordinates = fromLonLat([this.pointCoordinatesValue.longitude, this.pointCoordinatesValue.latitude])
-    return new Feature({
-      geometry: new Point(coordinates),
+    let coordinates = this.fromLonLat([this.pointCoordinatesValue.longitude, this.pointCoordinatesValue.latitude])
+    return new this.Feature({
+      geometry: new this.Point(coordinates),
       ...this.pointCoordinatesValue,
     })
   }
