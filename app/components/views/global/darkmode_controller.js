7431f17f4985cf9f27e32ae1e9e2a0eac119f5d5
skyler <pkphung2001@gmail.com>
2024-08-29 01:00:28 +0700

    update

---
+++ app/components/views/global/darkmode_controller.js
@@ -0,0 +1,24 @@
+import { icon, tab } from "../../../javascript/controllers/components";
+import ApplicationController from "../../../javascript/controllers/application_controller";
+
+export default class DarkmodeController extends ApplicationController {
+
+  initAction() {
+    this.addAction(this.element, `click->${this.identifier}#darkmodeToggle`)
+  }
+
+  darkmodeToggle() {
+    this.htmlTag.classList.toggle('dark')
+  }
+}
+
+export const DarkmodeComponent = () => {
+  return (
+    tab({controller: "views--global--darkmode", klass: 'flex justify-center items-center w-fit h-fit', action: {listener: 'click', action: 'tabNext'}}, () => (
+      `
+        <div>${icon({variant: ['outline', 'moon']})}</div>
+        <div>${icon({variant: ['outline', 'sun']})}</div>
+      `
+    ))
+  )
+}
--- app/components/views/rs/header.js
+++ app/components/views/rs/header.js
@@ -1,8 +1,11 @@
+import { DarkmodeComponent } from "../global/darkmode_controller"
+
 export const header = () => {
   return `
     <div class="flex flex-row justify-between w-full h-full py-5 px-10">
       Header
     </div>
+    ${DarkmodeComponent()}
     <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"></hr>
   `
 }
