b2c48c41fd2d1f8704ca70bb33b276b782761c5c
skyler <pkphung2001@gmail.com>
2024-08-28 20:09:27 +0700

    update

---
+++ app/components/views/rs/rs_condos/new_component.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Views::Rs::RsCondos::NewComponent < ApplicationComponent
+  # def call
+  #   content_tag :h1, "Hello world!", data: { controller: "views--rs--rs-condos--new-component" }
+  # end
+
+end
