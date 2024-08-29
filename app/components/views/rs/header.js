ec528110a10021a51295b95c20f25e609c8aea68
skyler <pkphung2001@gmail.com>
2024-08-30 00:33:11 +0700

    update

--- app/components/views/rs/header.js
+++ app/components/views/rs/header.js
@@ -3,9 +3,17 @@ import { Views_Global_DarkmodeComponent } from "../global/darkmode_controller"
 export const Views_Rs_Header = () => {
   return `
     <div class="flex flex-row justify-between w-full h-full py-5 px-10">
-      Header
+      <div>
+        Logo
+      </div>
+      <div>
+        Nav
+      </div>
+      <div class="flex flex-row">
+        ${Views_Global_DarkmodeComponent()}
+        Authentication
+      </div>
     </div>
-    ${Views_Global_DarkmodeComponent()}
     <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"></hr>
   `
 }
