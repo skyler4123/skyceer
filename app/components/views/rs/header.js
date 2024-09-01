a24f00d70d051f93093ee9a3e7e9b19a968bf121
skyler <pkphung2001@gmail.com>
2024-09-01 07:06:04 +0700

    update

--- app/components/views/rs/header.js
+++ app/components/views/rs/header.js
@@ -1,10 +1,12 @@
+import { icon } from "../../../javascript/controllers/components"
 import { Views_Global_DarkmodeComponent } from "../global/darkmode_controller"
 
 export const Views_Rs_Header = () => {
   return `
     <div class="flex flex-row justify-between w-full h-full py-5 px-10">
-      <div>
-        Logo
+      <div class="flex flex-row gap-x-2 justify-center items-center">
+        ${icon({variant: ["outline", "rectangle-group"], klass: "h-10 w-10"})}
+        <div>Skyceer</div>
       </div>
       <div>
         Nav
