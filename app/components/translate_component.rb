543530cbfcd211fb891d051827bbbaa0789f63b7
skyler <pkphung2001@gmail.com>
2024-09-14 22:05:26 +0700

    update

---
+++ app/components/translate_component.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class TranslateComponent < ApplicationComponent
+  # def call
+  #   content_tag :h1, "Hello world!", data: { controller: "translate-component" }
+  # end
+
+end
