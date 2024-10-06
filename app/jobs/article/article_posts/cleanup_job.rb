129dc58a5a86776bb05e9a88e66ee12c8610d1c9
skyler <pkphung2001@gmail.com>
2024-10-07 03:08:46 +0700

    update

---
+++ app/jobs/article/article_posts/cleanup_job.rb
@@ -0,0 +1,11 @@
+class Article::ArticlePosts::CleanupJob < ApplicationJob
+  # queue_as :default
+  # self.queue_adapter = :solid_queue
+  CLEANUP_TIME=60 # seconds
+
+  def perform
+    cleanup_time = CLEANUP_TIME.seconds.ago
+    cleanup_article_posts = ArticlePost.where(:created_at.gt => cleanup_time, :updated_at.gt => cleanup_time, title: "", content: "")
+    cleanup_article_posts.destroy_all
+  end
+end
