d1c9242332c01211aa529dea6be72eef74cf830a
skyler <pkphung2001@gmail.com>
2024-10-24 04:53:39 +0700

    update

--- app/packages/estate/controllers/estate_houses_controller.rb
+++ app/packages/estate/controllers/estate_houses_controller.rb
@@ -23,11 +23,13 @@ class EstateHousesController < EstateController
 
   # POST /estate_houses or /estate_houses.json
   def create
-    debugger
-    return
     @estate_house = EstateHouse.new(estate_house_params)
+    if params[:estate_house][:address]
+      @address = Address.create(address_params[:address])
+      @estate_house.address = @address
+    end
     @estate_house.estate_user_id = Current.estate_user_id
-
+    debugger
     respond_to do |format|
       if @estate_house.save
         format.html { redirect_to @estate_house, notice: "Estate House was successfully created." }
@@ -70,6 +72,11 @@ class EstateHousesController < EstateController
 
     # Only allow a list of trusted parameters through.
     def estate_house_params
-      params.require(:estate_house).permit(:name, :price_cents, :longitude, :latitude, images: [], address: [:unit_number, :street_number, :address_line_1, :address_line_2, :city, :country_code, :postal_code])
+      # params.require(:estate_house).permit(:name, :price_cents, :longitude, :latitude, images: [], address: [:unit_number, :street_number, :address_line_1, :address_line_2, :city, :country_code, :postal_code])
+      params.require(:estate_house).permit(:name, :price_cents, :longitude, :latitude, images: [])
+    end
+
+    def address_params
+      params.require(:estate_house).permit(address: [:unit_number, :street_number, :address_line_1, :address_line_2, :city, :country_code, :postal_code])
     end
 end
