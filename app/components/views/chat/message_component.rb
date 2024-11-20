a499afb3a5926b16ee42caa1036c70feb63d7db0
skyler <pkphung2001@gmail.com>
2024-11-20 20:51:32 +0700

    update

--- app/components/views/chat/message_component.rb
+++ app/components/views/chat/message_component.rb
@@ -3,12 +3,12 @@
 class Views::Chat::MessageComponent < ApplicationComponent
 
   erb_template <<-ERB
-      <%= [@user_id, @content] %>
+      <%= [@user_id, @message_content] %>
   ERB
 
-  def initialize(user_id:, content:)
+  def initialize(user_id:, message_content:)
     @user_id = user_id
-    @content = content
+    @message_content = message_content
   end
   # def call
   #   content_tag :h1, "Hello world!", data: { controller: "views--chat--message-component" }
---
+++ app/jobs/chat/chat_messages/turbo_stream_broadcast_job.rb
@@ -0,0 +1,11 @@
+class Chat::ChatMessages::TurboStreamBroadcastJob < ApplicationJob
+  queue_as :turbo_stream_chat_messages_broadcast
+
+  def perform(chat_conversation_id, user_id, content)
+    Turbo::StreamsChannel.broadcast_append_to(
+      chat_conversation_id,
+      target: chat_conversation_id,
+      html: ApplicationController.render(Views::Chat::MessageComponent.new(user_id: user_id, message_content: content), layout: false)
+    )
+  end
+end
