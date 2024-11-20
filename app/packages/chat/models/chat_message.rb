94548255636509c312cd9ddb19d100a2d7f240b1
skyler <pkphung2001@gmail.com>
2024-11-20 22:46:52 +0700

    update

--- app/packages/chat/models/chat_message.rb
+++ app/packages/chat/models/chat_message.rb
@@ -12,11 +12,11 @@ class ChatMessage
     # Turbo::StreamsChannel.broadcast_append_to(self.chat_conversation.id, target: 'chat_messages', partial: 'chat_messages/chat_message', locals: { chat_message: self })
     # Turbo::StreamsChannel.broadcast_append_to(self.chat_conversation.id, target: 'chat_messages', html: ApplicationController.render(TurboStreams::ChatMessageComponent.new(chat_message: self)))
     # Turbo::StreamsChannel.broadcast_append_to(self.chat_conversation.id, target: 'chat_messages', html: TurboStreams::ChatMessageComponent.new(chat_message: self).render_in(ActionController::Base.new.view_context))
-    # Turbo::StreamsChannel.broadcast_append_to(self.chat_conversation.id, target: self.chat_conversation.id, html: render_component)
-    Chat::ChatMessages::TurboStreamBroadcastJob.perform_later(self.chat_conversation.id, self.user_id, self.content)
+    Turbo::StreamsChannel.broadcast_append_to(self.chat_conversation.id, target: self.chat_conversation.id, html: render_component)
+    # Chat::ChatMessages::TurboStreamBroadcastJob.perform_later(self.chat_conversation.id, self.user_id, self.content)
   end
 
-  # def render_component
-  #   ApplicationController.render(Views::Chat::MessageComponent.new(chat_message: self), layout: false)
-  # end
+  def render_component
+    ApplicationController.render(Views::Chat::MessageComponent.new(user_id: self.user_id, message_content: self.content), layout: false)
+  end
 end
