1453f2dea0e260543c0c506abc8ecaf794251442
skyler <pkphung2001@gmail.com>
2024-11-23 13:35:58 +0700

    update

--- spec/features/education/education_schools/index_spec.rb
+++ spec/features/education/education_schools/index_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "education_schools#index", type: :feature, js: true do
   context "role: admin" do
     let!(:user) { create(:user, role: :admin) }
 
-    it "will be redirect to root_pathn" do
+    it "will not be redirected" do
       sign_in(user: user)
       visit education_schools_path
       expect(page).to have_current_path(education_schools_path)
