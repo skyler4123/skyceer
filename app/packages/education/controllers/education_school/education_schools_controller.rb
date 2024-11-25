905953194d6651cd31772ef7db3290d6e6038b1e
skyler <pkphung2001@gmail.com>
2024-11-26 05:12:03 +0700

    update

--- app/packages/education/controllers/education_school/education_schools_controller.rb
+++
@@ -1,9 +0,0 @@
-class EducationSchool::EducationSchoolsController < EducationsController
-
-  # GET /education_schools or /education_schools.json
-  def index
-    @education_schools = EducationSchool.where(user_id: Current.user_id)
-    authorize [:education_school, @education_schools]
-  end
-
-end
