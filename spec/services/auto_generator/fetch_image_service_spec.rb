7e837fea4e6b6fe355ad4842b76a6e2ad92773f6
skyler <pkphung2001@gmail.com>
2024-11-30 13:07:48 +0700

    update

--- spec/services/auto_generator/fetch_image_service_spec.rb
+++ spec/services/auto_generator/fetch_image_service_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe AutoGenerator::FetchImageService do
     
     it 'return file', :vcr do
       expect(result.class).to eq(Tempfile)
-      expect(result.size).to be_an_instance_of(Numeric)
+      expect(result.size).to be_an_instance_of(Integer)
       expect(result.size > 0).to be_truthy
     end
   end
--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -5,4 +5,8 @@ VCR.configure do |config|
   config.default_cassette_options = {
     serialize_with: :json
   }
+  config.preserve_exact_body_bytes do |http_message|
+    http_message.body.encoding == Encoding::BINARY ||
+    !http_message.body.valid_encoding?
+  end
 end
