36d1104c22a1e4c9c9ce81de5855ec2a163a3d85
skyler <pkphung2001@gmail.com>
2024-12-06 21:13:12 +0700

    update

---
+++ app/javascript/controllers/education/education_students/new_controller.js
@@ -0,0 +1,9 @@
+import Education_LayoutController from "../layout_controller";
+
+export default class extends Education_LayoutController {
+  
+  // connect() {
+  //   console.log("Hello, Stimulus!", this);
+  // }
+
+}
