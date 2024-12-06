2cde03e70e4351ca964d6c5eb28ab53c3e1ef10c
skyler <pkphung2001@gmail.com>
2024-12-06 22:34:37 +0700

    update

--- app/packages/education/models/education_teacher.rb
+++ app/packages/education/models/education_teacher.rb
@@ -4,4 +4,6 @@ class EducationTeacher < ApplicationRecord
 
   belongs_to :user
   belongs_to :education_school
+
+  validates :user_id, uniqueness: { scope: :education_school_id, message: "should only one per school" }
 end
