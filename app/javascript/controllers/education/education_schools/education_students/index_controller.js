cd5530c15dc36b69b2f41254db8a82377f8ecb99
skyler <pkphung2001@gmail.com>
2024-12-16 03:44:21 +0700

    update

--- app/javascript/controllers/education/education_schools/education_students/index_controller.js
+++ app/javascript/controllers/education/education_schools/education_students/index_controller.js
@@ -7,21 +7,9 @@ export default class extends Education_LayoutController {
   }
   init() {
     console.log(this.contentData())
-  }
-  connect() {
-    // console.log("Hello, Stimulus!", this);
-    var tabledata = [
-        {id:1, name:"Oli Bob", progress:12, gender:"male", rating:1, col:"red", dob:"19/02/1984", car:1},
-        {id:2, name:"Mary May", progress:1, gender:"female", rating:2, col:"blue", dob:"14/05/1982", car:true},
-        {id:3, name:"Christine Lobowski", progress:42, gender:"female", rating:0, col:"green", dob:"22/05/1982", car:"true"},
-        {id:4, name:"Brendon Philips", progress:100, gender:"male", rating:1, col:"orange", dob:"01/08/1980"},
-        {id:5, name:"Margret Marmajuke", progress:16, gender:"female", rating:5, col:"yellow", dob:"31/01/1999"},
-        {id:6, name:"Frank Harbours", progress:38, gender:"male", rating:4, col:"red", dob:"12/05/1966", car:1},
-    ];
 
-    //initialize table
     var table = new Tabulator(this.contentDataTarget, {
-      data:tabledata,           //load row data from array
+      data: this.contentData(),           //load row data from array
       layout:"fitColumns",      //fit columns to width of table
       responsiveLayout:"hide",  //hide columns that don't fit on the table
       addRowPos:"top",          //when adding a new row, add it to the top of the table
@@ -38,13 +26,14 @@ export default class extends Education_LayoutController {
       },
       columns:[                 //define the table columns
           {title:"Name", field:"name", editor:"input"},
-          {title:"Task Progress", field:"progress", hozAlign:"left", formatter:"progress", editor:true},
-          {title:"Gender", field:"gender", width:95, editor:"select", editorParams:{values:["male", "female"]}},
-          {title:"Rating", field:"rating", formatter:"star", hozAlign:"center", width:100, editor:true},
-          {title:"Color", field:"col", width:130, editor:"input"},
-          {title:"Date Of Birth", field:"dob", width:130, sorter:"date", hozAlign:"center"},
-          {title:"Driver", field:"car", width:90,  hozAlign:"center", formatter:"tickCross", sorter:"boolean", editor:true},
+          {title:"Created At", field:"created_at", width:130, sorter:"date", hozAlign:"center"},
+          {title:"Updated At", field:"updated_at", width:130, sorter:"date", hozAlign:"center"},
+
       ],
     });
   }
+
+  // connect() {
+  //   // console.log("Hello, Stimulus!", this);
+  // }
 }
