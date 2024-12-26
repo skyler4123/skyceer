d1e22bd5c104f1bd057a81e46542819f3be2d517
skyler <pkphung2001@gmail.com>
2024-12-26 18:58:55 +0700

    update

--- app/packages/education/controllers/education_teachers_controller.rb
+++ app/packages/education/controllers/education_teachers_controller.rb
@@ -1,11 +1,22 @@
+require 'pagy/extras/elasticsearch_rails'
+
 class EducationTeachersController < EducationsController
   before_action :set_education_schools, only: %i[ index ]
   before_action :set_education_teacher, only: %i[ show edit update destroy ]
 
   # GET /education_teachers or /education_teachers.json
   def index
-    @education_teachers = EducationTeacher.all.includes(:education_school).where(education_school: @education_schools)
-    @pagy, @education_teachers = pagy(@education_teachers)
+    if params[:full_text_search].present?
+      @education_teachers = EducationTeacher.search(params[:full_text_search]).records
+    elsif params[:education_school_id].present?
+      @education_teachers = EducationTeacher.where(education_school_id: params[:education_school_id])
+    else
+      @education_teachers = EducationTeacher.all.includes(:education_school).where(education_school: @education_schools)
+    end
+    respond_to do |format|
+      format.html { @pagy, @education_teachers = pagy(@education_teachers) }
+      format.json { render json: @education_teachers }
+    end
   end
 
   # GET /education_teachers/1 or /education_teachers/1.json
