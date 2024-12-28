a9ed0e89d89bcc1a5d15871a08fc48ea1edb8454
skyler <pkphung2001@gmail.com>
2024-12-28 21:22:37 +0700

    update

--- app/services/auto_generator/payment_service.rb
+++ app/services/auto_generator/payment_service.rb
@@ -3,11 +3,12 @@ class AutoGenerator::PaymentService
     self.payment_method
     self.payment_method_appointment
     self.payment_discount
+    self.payment_discount_appointment
     self.payment_order
     # OK
 
     self.payment_item_appointment
-    5.times { self.payment_log }
+    self.payment_log
   end
 
   def self.payment_customer
@@ -50,6 +51,15 @@ class AutoGenerator::PaymentService
     end
   end
 
+  def self.payment_discount_appointment
+    PaymentDiscount.all.each do |payment_discount|
+      payment_user = payment_discount.payment_user
+      PaymentDiscountAppointment.create!(
+        payment_user: payment_user,
+        payment_discount: payment_discount
+      )
+    end
+  end
   def self.payment_order
     PaymentCustomer.all.each do |payment_customer|
       payment_user = PaymentUser.all.sample
@@ -70,9 +80,9 @@ class AutoGenerator::PaymentService
   end
   
   def self.payment_item_appointment
-    PaymentOrder.all.each do |payment_order|
-      payment_user = payment_order.payment_user
-      payment_item = payment_user.payment_items.sample
+    PaymentItem.all.each do |payment_item|
+      payment_user = payment_item.payment_user
+      payment_order = payment_user.payment_orders.sample
       PaymentItemAppointment.create!(
         payment_item: payment_item,
         payment_order: payment_order,
