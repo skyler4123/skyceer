94814fa000e0b8ef603d4a67fac800520d166711
skyler <pkphung2001@gmail.com>
2025-01-05 13:58:21 +0700

    update

--- app/models/session_application_record.rb
+++ app/models/session_application_record.rb
@@ -1,4 +1,4 @@
 class SessionApplicationRecord < ApplicationRecord
-  self.abstract_class = true
-  connects_to database: { writing: :session, reading: :session }
+  # self.abstract_class = true
+  # connects_to database: { writing: :session, reading: :session }
 end
--- app/services/auto_generator/seed_service.rb
+++ app/services/auto_generator/seed_service.rb
@@ -39,66 +39,25 @@ class AutoGenerator::SeedService
   end
 
   def self.puts_count
-    user_count = User.count
-    category_count = Category.count
-    calendar_group_count = CalendarGroup.count
-    calendar_event_count = CalendarEvent.count
+    count_array = []
+    skip_models = [ApplicationRecord, Current, Demo, Session, SessionApplicationRecord, EstateUser, VehicleUser, VehicleStore, VehicleCar]
+    Dir[Rails.root.to_s + '/app/**/models/*.rb'].each do |file| 
+      # Given string
+      file_name = file.split('/').last
+      
+      # Step 1: Remove the file extension
+      class_name = file_name.sub('.rb', '')
+    
+      # Step 2: Convert the string to CamelCase
+      class_name = class_name.camelize
+    
+      # Step 3: Use `constantize` to get the model class
+      model_class = class_name.constantize
+      next if skip_models.include?(model_class)
 
-    education_category_count = EducationCategory.count
-    education_school_count = EducationSchool.count
-    education_class_count = EducationClass.count
-    education_room_count = EducationRoom.count
-    education_admin_count = EducationAdmin.count
-    education_teacher_count = EducationTeacher.count
-    education_student_count = EducationStudent.count
-
-    chat_conversation_count = ChatConversation.count
-    article_post_count = ArticlePost.count
-    estate_condo_count = EstateCondo.count
-    estate_hotel_count = EstateHotel.count
-    estate_house_count = EstateHouse.count
-    report_ticket_count = ReportTicket.count
-    vehicle_store_count = VehicleStore.count
-    vehicle_car_count = VehicleCar.count
-
-    payment_user_count = PaymentUser.count
-    payment_customer_count = PaymentCustomer.count
-    payment_order_count = PaymentOrder.count
-    payment_method_count = PaymentMethod.count
-    payment_discount_count = PaymentDiscount.count
-    payment_log_count = PaymentLog.count
-    payment_item_count = PaymentItem.count
-    payment_invoice_count = PaymentInvoice.count
-
-    puts "Users: ", user_count
-    puts "Categories: ", category_count
-    puts "Calendar Groups: ", calendar_group_count
-    puts "Calendar Events: ", calendar_event_count
-
-    puts "Education Categories: ", education_category_count
-    puts "Education Schools: ", education_school_count
-    puts "Education Classes: ", education_class_count
-    puts "Education Rooms: ", education_room_count
-    puts "Education Admins: ", education_admin_count
-    puts "Education Teachers: ", education_teacher_count
-    puts "Education Students: ", education_student_count
-
-    puts "Chat Conversations: ", chat_conversation_count
-    puts "Article Posts: ", article_post_count
-    puts "Estate Condos: ", estate_condo_count
-    puts "Estate Hotels: ", estate_hotel_count
-    puts "Estate Houses: ", estate_house_count
-    puts "Report Tickets: ", report_ticket_count
-    puts "Vehicle Stores: ", vehicle_store_count
-    puts "Vehicle Cars: ", vehicle_car_count
-
-    puts "Payment Users: ", payment_user_count
-    puts "Payment Customers: ", payment_customer_count
-    puts "Payment Orders: ", payment_order_count
-    puts "Payment Methods: ", payment_method_count
-    puts "Payment Discounts: ", payment_discount_count
-    puts "Payment Logs: ", payment_log_count
-    puts "Payment Items: ", payment_item_count
-    puts "Payment Invoices: ", payment_invoice_count
+      # Push model class count to count_array
+      count_array << {model_class.to_s => model_class.count}
+    end
+    puts count_array
   end
 end
