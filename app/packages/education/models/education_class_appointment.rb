922913746af7a64226ed98de7bbd55c06ce8b12c
skyler <pkphung2001@gmail.com>
2025-01-12 19:21:31 +0700

    update

--- app/packages/education/models/education_class_appointment.rb
+++ app/packages/education/models/education_class_appointment.rb
@@ -1,4 +1,18 @@
 class EducationClassAppointment < ApplicationRecord
   belongs_to :education_class
   belongs_to :education_class_appointmentable, polymorphic: true
+
+  validates :education_class_id, presence: true, uniqueness: { scope: [:education_class_appointmentable_id, :education_class_appointmentable_type] }
+  # Can be move to background job
+  after_create :create_education_exam_appointments_on_students
+
+  private
+
+  def create_education_exam_appointments_on_students
+    return if (self.education_class_appointmentable_type != 'EducationExam')
+    education_exam = self.education_class_appointmentable
+    education_class.education_students.each do |education_student|
+      education_student.education_exams << education_exam
+    end
+  end
 end
