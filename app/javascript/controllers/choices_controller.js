0a0b76a3900fc7ddb3a4d8cadaa2d99c2bf7516a
skyler <pkphung2001@gmail.com>
2025-01-14 22:48:58 +0700

    update

--- app/javascript/controllers/choices_controller.js
+++ app/javascript/controllers/choices_controller.js
@@ -1,3 +1,14 @@
+//  HOW TO USE
+//  <form action="/">
+//    <select data-controller="hello choices" name="cars" id="cars" multiple>
+//      <option value="volvo">Volvo</option>
+//      <option selected value="saab">Saab</option>
+//      <option selected value="opel">Opel</option>
+//      <option value="audi">Audi</option>
+//    </select>
+//    <input type="submit">
+//  </form>
+
 import "choices"
 
 import { Controller } from "@hotwired/stimulus"
