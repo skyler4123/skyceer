f66f4737d45c87d5b4df862cacd88609fda53992
skyler <pkphung2001@gmail.com>
2025-01-25 23:21:24 +0700

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,24 +13,38 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  before_action :set_current_session
   before_action :set_current_request_details
   before_action :authenticate
+
+  helper_method :current_user, :is_signed_in
 
   private
     def current_user
       Current.user
     end
     
-    def authenticate
+    def set_current_session
       if session_record = Session.find_by_id(cookies.signed[:session_token])
         Current.session = session_record
-      else
-        redirect_to sign_in_path
       end
+    end
+
+    def authenticate
+      # if session_record = Session.find_by_id(cookies.signed[:session_token])
+      #   Current.session = session_record
+      # else
+      #   redirect_to sign_in_path
+      # end
+      redirect_to sign_in_path unless Current.session
     end
 
     def set_current_request_details
       Current.user_agent = request.user_agent
       Current.ip_address = request.ip
+    end
+
+    def is_signed_in
+      !!current_user
     end
 end
--- app/javascript/controllers/education/home/index_controller.js
+++ app/javascript/controllers/education/home/index_controller.js
@@ -3,23 +3,23 @@ import ApplicationController from "../../application_controller"
 
 // Connects to data-controller="education--home--index"
 export default class extends ApplicationController {
-  static targets = ['header', 'main', 'aside', 'content', 'footer']
+  // static targets = ['header', 'main', 'aside', 'content', 'footer']
 
-  init() {
-    this.initHeader()
-    this.initFooter()
-    this.initMain()
-  }
+  // init() {
+  //   this.initHeader()
+  //   this.initFooter()
+  //   this.initMain()
+  // }
 
-  initHeader() {
-    this.headerTarget.setAttribute('data-controller', "education--header")
-  }
-  initMain() {
-    this.mainTarget.setAttribute('data-controller', "education--main")
-  }
+  // initHeader() {
+  //   this.headerTarget.setAttribute('data-controller', "education--header")
+  // }
+  // initMain() {
+  //   this.mainTarget.setAttribute('data-controller', "education--main")
+  // }
 
-  initFooter() {
-    this.footerTarget.setAttribute('data-controller', "education--footer")
-  }
+  // initFooter() {
+  //   this.footerTarget.setAttribute('data-controller', "education--footer")
+  // }
 
 }
