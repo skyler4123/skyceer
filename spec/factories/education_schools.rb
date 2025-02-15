8c0feb51e0e36d96ff3b7065549dbda0b6231b10
skyler <pkphung2001@gmail.com>
2025-02-16 01:19:03 +0700

    update

--- spec/factories/education_schools.rb
+++ spec/factories/education_schools.rb
@@ -1,10 +1,12 @@
 FactoryBot.define do
   factory :education_school do
-    # education_user { nil }
-    # have association with user
-    # user { create(:user) }
-    # name { "MyString" }
-    association :user, factory: :user
-    name { "MyString" }
+    association :user
+    association :address
+    name { "Sample School" }
+    email { "school@example.com" }
+    category { "Primary" }
+    discarded_at { nil }
+    created_at { Time.now }
+    updated_at { Time.now }
   end
 end
