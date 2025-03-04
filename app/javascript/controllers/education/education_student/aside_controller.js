9a61cf98d2e1cd349d5aa9332703a3809275c9ab
skyler <pkphung2001@gmail.com>
2025-03-04 08:01:53 +0700

    update

--- app/javascript/controllers/education/education_student/aside_controller.js
+++ app/javascript/controllers/education/education_student/aside_controller.js
@@ -4,7 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class Education_EducationStudent_AsideController extends Controller {
 
   initialize() {
-    this.element.className = "flex w-1/4 h-screen bg-slate-800 pr-4 pl-4 pt-4"
+    this.element.className = "flex w-1/4 h-screen bg-gray-800 pr-4 pl-4 pt-4"
     this.element.innerHTML = this.defaultHTML()
   }
 
@@ -19,7 +19,7 @@ export default class Education_EducationStudent_AsideController extends Controll
                 <div class="w-6 h-6 flex justify-center items-center mr-4">
                   ${data.icon}
                 </div>
-                <div class="text-slate-200 leading-6">${data.name}</div>
+                <div class="text-gray-200 leading-6">${data.name}</div>
               </a>
               <hr class="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700">
             </li>
@@ -35,7 +35,7 @@ export default class Education_EducationStudent_AsideController extends Controll
         name: "School",
         url: `/education_schools`,
         icon: `
-          <svg class="stroke-slate-200" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <svg class="w-full h-full stroke-gray-200" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M6 15.5002H7M6 18.5002H7M17 18.5002H18M17 15.5002H18M10 22.0002V18.0002C10 16.8956 10.8954 16.0002 12 16.0002C13.1046 16.0002 14 16.8956 14 18.0002V22.0002M12 5H16.84C16.896 5 16.924 5 16.9454 4.9891C16.9642 4.97951 16.9795 4.96422 16.9891 4.9454C17 4.92401 17 4.89601 17 4.84V2.16C17 2.10399 17 2.07599 16.9891 2.0546C16.9795 2.03578 16.9642 2.02049 16.9454 2.0109C16.924 2 16.896 2 16.84 2H12.16C12.104 2 12.076 2 12.0546 2.0109C12.0358 2.02049 12.0205 2.03578 12.0109 2.0546C12 2.07599 12 2.10399 12 2.16V5ZM12 5V7.69092M12.03 12.25H12.0375M12 7.69092C12.1947 7.69092 12.3895 7.71935 12.5779 7.77623C13.0057 7.90536 13.3841 8.24585 14.1407 8.92681L17 11.5002L18.5761 11.8942C19.4428 12.1109 19.8761 12.2192 20.1988 12.4608C20.4834 12.674 20.7061 12.9592 20.8439 13.2871C21 13.6587 21 14.1053 21 14.9987V18.8002C21 19.9203 21 20.4804 20.782 20.9082C20.5903 21.2845 20.2843 21.5905 19.908 21.7822C19.4802 22.0002 18.9201 22.0002 17.8 22.0002H6.2C5.0799 22.0002 4.51984 22.0002 4.09202 21.7822C3.71569 21.5905 3.40973 21.2845 3.21799 20.9082C3 20.4804 3 19.9203 3 18.8002V14.9987C3 14.1053 3 13.6587 3.15613 13.2871C3.29388 12.9592 3.51657 12.674 3.80124 12.4608C4.12389 12.2192 4.55722 12.1109 5.42388 11.8942L7 11.5002L9.85931 8.92681C10.6159 8.24584 10.9943 7.90536 11.4221 7.77623C11.6105 7.71935 11.8053 7.69092 12 7.69092ZM12.03 13C11.6158 13 11.28 12.6642 11.28 12.25C11.28 11.8358 11.6158 11.5 12.03 11.5C12.4442 11.5 12.78 11.8358 12.78 12.25C12.78 12.6642 12.4442 13 12.03 13Z" stroke-linecap="round" stroke-linejoin="round"/>
           </svg>`
       },
@@ -43,7 +43,7 @@ export default class Education_EducationStudent_AsideController extends Controll
         name: "Teachers",
         url: `/education_teachers`,
         icon: `
-          <svg class="fill-slate-200" height="800px" width="800px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 299.97 299.97" xml:space="preserve">
+          <svg class="w-full h-full fill-gray-200" height="800px" width="800px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 299.97 299.97" xml:space="preserve">
             <g>
               <g>
                 <g>
@@ -58,7 +58,7 @@ export default class Education_EducationStudent_AsideController extends Controll
         name: "Students",
         url: `/education_students`,
         icon: `
-          <svg class="fill-slate-200" height="800px" width="800px" version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve">
+          <svg class="w-full h-full fill-gray-200" height="800px" width="800px" version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve">
             <g>
               <path d="M473.61,63.16L276.16,2.927C269.788,0.986,263.004,0,256.001,0c-7.005,0-13.789,0.986-20.161,2.927   L38.386,63.16c-3.457,1.064-5.689,3.509-5.689,6.25c0,2.74,2.232,5.186,5.691,6.25l91.401,27.88v77.228   c0.023,39.93,13.598,78.284,38.224,107.981c11.834,14.254,25.454,25.574,40.483,33.633c15.941,8.564,32.469,12.904,49.124,12.904   c16.646,0,33.176-4.34,49.126-12.904c22.597-12.143,42.04-31.646,56.226-56.39c14.699-25.683,22.471-55.155,22.478-85.224v-78.214   l45.244-13.804v64.192c-6.2,0.784-11.007,6.095-11.007,12.5c0,5.574,3.649,10.404,8.872,12.011l-9.596,63.315   c-0.235,1.576,0.223,3.168,1.262,4.386c1.042,1.204,2.554,1.902,4.148,1.902h36.273c1.592,0,3.104-0.699,4.148-1.91   c1.036-1.203,1.496-2.803,1.262-4.386l-9.596-63.307c5.223-1.607,8.872-6.436,8.872-12.011c0-6.405-4.81-11.716-11.011-12.5V81.544   l19.292-5.885c3.457-1.064,5.691-3.517,5.691-6.25C479.303,66.677,477.069,64.223,473.61,63.16z M257.62,297.871   c-10.413,0-20.994-2.842-31.448-8.455c-16.194-8.649-30.908-23.564-41.438-42.011c-4.854-8.478-8.796-17.702-11.729-27.445   c60.877-10.776,98.51-49.379,119.739-80.97c10.242,20.776,27.661,46.754,54.227,58.648c-3.121,24.984-13.228,48.812-28.532,67.212   c-8.616,10.404-18.773,18.898-29.375,24.573C278.606,295.029,268.025,297.871,257.62,297.871z"/>
               <path d="M373.786,314.23l-1.004-0.629l-110.533,97.274L151.714,313.6l-1.004,0.629   c-36.853,23.036-76.02,85.652-76.02,156.326v0.955l0.846,0.45C76.291,472.365,152.428,512,262.249,512   c109.819,0,185.958-39.635,186.712-40.038l0.846-0.45v-0.955C449.808,399.881,410.639,337.265,373.786,314.23z"/>
@@ -69,7 +69,7 @@ export default class Education_EducationStudent_AsideController extends Controll
         name: "Classes",
         url: `/education_classes`,
         icon: `
-          <svg class="fill-slate-200" height="800px" width="800px" version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve">
+          <svg class="w-full h-full fill-gray-200" height="800px" width="800px" version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve">
             <g>
               <path d="M145.764,98.011c27.058,0,49.002-21.943,49.002-49.009C194.766,21.935,172.822,0,145.764,0   c-27.066,0-49.009,21.935-49.009,49.002C96.755,76.068,118.698,98.011,145.764,98.011z"/>
               <circle cx="110.089" cy="377.876" r="48.646"/>
@@ -86,7 +86,7 @@ export default class Education_EducationStudent_AsideController extends Controll
         name: "Score Boards",
         url: `/education_scoreboards`,
         icon: `
-          <svg class="fill-slate-200" width="800px" height="800px" viewBox="0 0 800 800" enable-background="new 0 0 800 800" id="GUIDE" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          <svg class="w-full h-full fill-gray-200" width="800px" height="800px" viewBox="0 0 800 800" enable-background="new 0 0 800 800" id="GUIDE" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
             <g>
             <path d="M774.721,110.344c4.668-10.011,0.337-21.91-9.674-26.578L649.241,29.765c-4.807-2.242-10.309-2.482-15.292-0.668   c-4.985,1.814-9.045,5.534-11.287,10.342l-59.898,128.455l-29.281-86.015c-2.76-8.104-10.372-13.555-18.934-13.555H121.323   c-6.015,0-11.71,2.707-15.508,7.37l-77.922,95.678c-4.352,5.344-5.646,12.551-3.425,19.075L217.865,758.55   c2.759,8.104,10.371,13.555,18.933,13.555H659.33c11.047,0,20-8.954,20-20s-8.953-20-20-20H251.116L71.339,204.007l94.295,0.02   c0.001,0,0.003,0,0.004,0c6.824,0,13.178-3.479,16.853-9.229c3.676-5.751,4.164-12.98,1.296-19.174l-31.162-67.3H500.23   l38.11,111.947L405.278,505.625c-1.536,3.292-2.108,6.908-1.749,10.463c-0.008,0.002-0.015,0.003-0.023,0.005l12.581,124.188   c0.711,7.012,5.059,13.132,11.446,16.11c2.69,1.255,5.576,1.874,8.45,1.874c3.948,0,7.875-1.168,11.247-3.461l103.225-70.188   c-0.002-0.003-0.003-0.006-0.004-0.009c2.886-1.958,5.303-4.688,6.884-8.078l59.123-126.789l105.126,308.81   c2.833,8.321,10.605,13.56,18.932,13.56c2.137,0,4.312-0.345,6.447-1.072c10.457-3.559,16.047-14.922,12.488-25.378   L640.883,397.363L774.721,110.344z M85.477,164.01l31.145-38.242l17.713,38.252L85.477,164.01z M650.463,74.47l79.554,37.096   l-18.285,39.214l-79.554-37.097L650.463,74.47z M446.85,547.077l52.008,24.251L452.5,602.85L446.85,547.077z M529.535,541.499   l-79.553-37.096l165.291-354.468l79.553,37.097L529.535,541.499z"/>
             <path d="M608.921,220.086L528.08,393.45c-4.668,10.011-0.338,21.91,9.674,26.578c2.734,1.275,5.609,1.879,8.438,1.879   c7.532,0,14.747-4.275,18.14-11.553l80.841-173.364c4.669-10.011,0.337-21.91-9.674-26.578   C625.49,205.743,613.589,210.075,608.921,220.086z"/>
@@ -103,14 +103,14 @@ export default class Education_EducationStudent_AsideController extends Controll
         name: "Time Tables",
         url: `/education_time_tables`,
         icon: `
-        <svg class="fill-slate-200" width="800px" height="800px" viewBox="0 0 32 32" data-name="Layer 13" id="Layer_13" xmlns="http://www.w3.org/2000/svg"><title/><path d="M28.55,6.57H26.42V4.93a0.5,0.5,0,1,0-1,0V6.57H19.81V5.06a0.5,0.5,0,0,0-1,0V6.57H13.19V5.06a0.5,0.5,0,0,0-1,0V6.57H6.58V5.06a0.5,0.5,0,1,0-1,0V6.57H3.45A2,2,0,0,0,1.5,8.52v17.1a2,2,0,0,0,1.95,2h25.1a2,2,0,0,0,1.95-2V8.52A2,2,0,0,0,28.55,6.57Zm-25.1,1H5.58V9.08a0.5,0.5,0,0,0,1,0V7.57h5.61V9.08a0.5,0.5,0,0,0,1,0V7.57h5.61V9.08a0.5,0.5,0,0,0,1,0V7.57h5.61V8.94a0.5,0.5,0,1,0,1,0V7.57h2.13a1,1,0,0,1,.95.95v2.94H2.5V8.52A1,1,0,0,1,3.45,7.57Zm25.1,19H3.45a1,1,0,0,1-.95-1V12.46h27V25.62A1,1,0,0,1,28.55,26.57Z"/><rect height="2.13" width="2.13" x="9.99" y="14.39"/><rect height="2.13" width="2.13" x="14.98" y="14.39"/><rect height="2.13" width="2.13" x="19.98" y="14.37"/><rect height="2.13" width="2.13" x="5" y="18.45"/><rect height="2.13" width="2.13" x="9.99" y="18.45"/><rect height="2.13" width="2.13" x="14.98" y="18.45"/><rect height="2.13" width="2.13" x="5" y="22.56"/><rect height="2.13" width="2.13" x="9.99" y="22.56"/><rect height="2.13" width="2.13" x="14.98" y="22.55"/><rect height="2.13" width="2.13" x="19.98" y="22.55"/><rect height="2.13" width="2.13" x="19.98" y="18.44"/><rect height="2.13" width="2.13" x="24.87" y="14.36"/><rect height="2.13" width="2.13" x="24.87" y="18.42"/></svg>
+        <svg class="w-full h-full fill-gray-200" width="800px" height="800px" viewBox="0 0 32 32" data-name="Layer 13" id="Layer_13" xmlns="http://www.w3.org/2000/svg"><title/><path d="M28.55,6.57H26.42V4.93a0.5,0.5,0,1,0-1,0V6.57H19.81V5.06a0.5,0.5,0,0,0-1,0V6.57H13.19V5.06a0.5,0.5,0,0,0-1,0V6.57H6.58V5.06a0.5,0.5,0,1,0-1,0V6.57H3.45A2,2,0,0,0,1.5,8.52v17.1a2,2,0,0,0,1.95,2h25.1a2,2,0,0,0,1.95-2V8.52A2,2,0,0,0,28.55,6.57Zm-25.1,1H5.58V9.08a0.5,0.5,0,0,0,1,0V7.57h5.61V9.08a0.5,0.5,0,0,0,1,0V7.57h5.61V9.08a0.5,0.5,0,0,0,1,0V7.57h5.61V8.94a0.5,0.5,0,1,0,1,0V7.57h2.13a1,1,0,0,1,.95.95v2.94H2.5V8.52A1,1,0,0,1,3.45,7.57Zm25.1,19H3.45a1,1,0,0,1-.95-1V12.46h27V25.62A1,1,0,0,1,28.55,26.57Z"/><rect height="2.13" width="2.13" x="9.99" y="14.39"/><rect height="2.13" width="2.13" x="14.98" y="14.39"/><rect height="2.13" width="2.13" x="19.98" y="14.37"/><rect height="2.13" width="2.13" x="5" y="18.45"/><rect height="2.13" width="2.13" x="9.99" y="18.45"/><rect height="2.13" width="2.13" x="14.98" y="18.45"/><rect height="2.13" width="2.13" x="5" y="22.56"/><rect height="2.13" width="2.13" x="9.99" y="22.56"/><rect height="2.13" width="2.13" x="14.98" y="22.55"/><rect height="2.13" width="2.13" x="19.98" y="22.55"/><rect height="2.13" width="2.13" x="19.98" y="18.44"/><rect height="2.13" width="2.13" x="24.87" y="14.36"/><rect height="2.13" width="2.13" x="24.87" y="18.42"/></svg>
         `,
       },
       {
         name: "Courses",
         url: `/education_courses`,
         icon: `
-          <svg class="fill-slate-200" width="800px" height="800px" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg">
+          <svg class="w-full h-full fill-gray-200" width="800px" height="800px" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg">
               <path d="M1750.21 0v1468.235h-225.882v338.824h169.412V1920H451.387c-82.447 0-161.506-36.141-214.701-99.388-43.934-51.953-67.652-116.33-67.652-182.965V282.353C169.034 126.494 295.528 0 451.387 0H1750.21Zm-338.823 1468.235H463.81c-89.223 0-166.136 59.86-179.576 140.047-1.242 9.036-2.259 18.07-2.259 27.106v2.26c0 40.658 13.553 77.928 40.659 109.552 32.753 38.4 79.059 59.859 128.753 59.859h960v-112.941H409.599v-112.942h1001.788v-112.94Zm225.882-1355.294H451.387c-92.725 0-169.412 75.67-169.412 169.412v1132.8c50.824-37.27 113.958-59.859 181.835-59.859h1173.46V112.941ZM1354.882 903.53v112.942H564.294V903.529h790.588Zm56.47-564.705v451.764H507.825V338.824h903.529Zm-112.94 112.94H620.765v225.883h677.647V451.765Z" fill-rule="evenodd"/>
           </svg>`
       },
@@ -118,7 +118,7 @@ export default class Education_EducationStudent_AsideController extends Controll
         name: "Subjects",
         url: `/education_subjects`,
         icon: `
-          <svg class="fill-slate-200" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="800px" height="800px" viewBox="0 0 31.314 31.314" xml:space="preserve">
+          <svg class="w-full h-full fill-gray-200" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="800px" height="800px" viewBox="0 0 31.314 31.314" xml:space="preserve">
             <g>
               <g>
                 <rect x="23.698" y="15.251" width="5.051" height="1.879"/>
@@ -133,7 +133,7 @@ export default class Education_EducationStudent_AsideController extends Controll
         name: "Exams",
         url: `/education_exams`,
         icon: `
-          <svg class="fill-slate-200" height="800px" width="800px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve">
+          <svg class="w-full h-full fill-gray-200" height="800px" width="800px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve">
             <g>
               <g>
                 <path d="M507.156,121.024l-22.113-22.112c-3.129-3.13-7.29-4.853-11.715-4.853s-8.587,1.723-11.715,4.853l-0.912,0.913V84.974    c0-18.566-15.105-33.671-33.671-33.671H33.671C15.105,51.304,0,66.408,0,84.974v342.052c0,18.566,15.105,33.671,33.671,33.671    H427.03c18.566,0,33.671-15.105,33.671-33.671V190.911l46.455-46.455C513.615,137.997,513.615,127.485,507.156,121.024z     M444.667,427.026c0,9.725-7.912,17.637-17.637,17.637H33.671c-9.725,0-17.637-7.912-17.637-17.637V84.974    c0-9.725,7.912-17.637,17.637-17.637H427.03c9.725,0,17.637,7.912,17.637,17.637v30.885l-35.274,35.274v-40.505    c0-3.557-2.344-6.689-5.758-7.692c-3.414-1-7.078,0.365-9.002,3.357l-31.737,49.369l-31.737-49.369    c-1.923-2.992-5.589-4.357-9.002-3.357c-3.413,1.003-5.758,4.135-5.758,7.692v85.513c0,4.427,3.589,8.017,8.017,8.017    s8.017-3.589,8.017-8.017v-58.217l23.72,36.899c1.475,2.294,4.016,3.681,6.744,3.681c2.728,0,5.269-1.387,6.744-3.681    l23.72-36.899v29.242L244.133,316.394h-47.988c-4.427,0-8.017,3.589-8.017,8.017s3.589,8.017,8.017,8.017h36.439l-24.443,52.377    h-80.407c-4.427,0-8.017,3.589-8.017,8.017s3.589,8.017,8.017,8.017h85.513c0.001,0,0.001,0,0.002,0    c1.147,0,2.304-0.246,3.387-0.753l64.135-29.93c0.847-0.396,1.618-0.935,2.279-1.596l36.132-36.132h65.09    c4.427,0,8.017-3.589,8.017-8.017s-3.589-8.017-8.017-8.017h-49.057l109.45-109.449V427.026z M248.323,336.618l21.128,21.128    l-39.615,18.487L248.323,336.618z M281.658,347.278l-22.867-22.867l188.884-188.884l22.868,22.868L281.658,347.278z     M495.819,133.118l-13.939,13.939l-22.868-22.868l13.939-13.939c0.208-0.207,0.547-0.207,0.756,0l22.112,22.111    C496.026,132.571,496.026,132.91,495.819,133.118z"/>
@@ -185,7 +185,7 @@ export default class Education_EducationStudent_AsideController extends Controll
         name: "Payments",
         url: `/payment_orders`,
         icon: `
-          <svg class="fill-slate-200" height="800px" width="800px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512.001 512.001" xml:space="preserve">
+          <svg class="w-full h-full fill-gray-200" height="800px" width="800px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512.001 512.001" xml:space="preserve">
             <g>
               <g>
                 <path d="M503.524,87.486h-92.897c-4.682,0-8.476,3.795-8.476,8.476c0,4.681,3.794,8.476,8.476,8.476h41.672    c3.604,21.854,20.895,39.144,42.749,42.749v98.963c-21.854,3.604-39.145,20.895-42.749,42.749H142.724    c-3.604-21.854-20.895-39.144-42.749-42.749v-98.963c21.854-3.604,39.144-20.895,42.749-42.749H381.49    c4.682,0,8.476-3.795,8.476-8.476c0-4.681-3.794-8.476-8.476-8.476H91.498c-4.681,0-8.476,3.795-8.476,8.476v201.412    c0,4.681,3.795,8.476,8.476,8.476h233.978v24.863H16.952V237.69c0-9.116,7.417-16.533,16.533-16.533h29.757    c4.681,0,8.476-3.795,8.476-8.476c0-4.681-3.795-8.476-8.476-8.476H33.484C15.022,204.207,0,219.227,0,237.691V391.03    c0,18.463,15.022,33.484,33.484,33.484h275.46c18.464,0,33.484-15.021,33.484-33.484v-85.179h161.096    c4.682,0,8.476-3.795,8.476-8.476V95.962C512,91.281,508.206,87.486,503.524,87.486z M99.974,104.438h25.456    c-3.12,12.488-12.968,22.336-25.456,25.456V104.438z M99.974,288.898v-25.456c12.488,3.12,22.336,12.968,25.456,25.456H99.974z     M325.476,363.487H83.754c-4.681,0-8.476,3.795-8.476,8.476s3.795,8.476,8.476,8.476h241.723v10.59    c0,9.117-7.416,16.533-16.531,16.533H33.484c-9.116,0-16.533-7.417-16.533-16.533v-10.59H52.11c4.681,0,8.476-3.795,8.476-8.476    s-3.795-8.476-8.476-8.476H16.952v-15.822h308.524V363.487z M495.048,288.898h-25.456c3.12-12.488,12.968-22.336,25.456-25.456    V288.898z M495.048,129.894c-12.488-3.12-22.336-12.968-25.456-25.456h25.456V129.894z"/>
