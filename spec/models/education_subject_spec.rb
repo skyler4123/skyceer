bf7edfdebc40edca6e8a405d177adfbba248d2be
skyler <pkphung2001@gmail.com>
2025-03-08 22:02:59 +0700

    update

--- spec/models/education_subject_spec.rb
+++ spec/models/education_subject_spec.rb
@@ -10,5 +10,6 @@ RSpec.describe EducationSubject, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
   end
 end
