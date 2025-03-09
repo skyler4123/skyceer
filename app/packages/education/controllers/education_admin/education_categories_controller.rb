be47fb283176dcea945bfb23662f1e1e256d24ae
skyler <pkphung2001@gmail.com>
2025-03-09 23:54:40 +0700

    update

--- app/packages/education/controllers/education_admin/education_categories_controller.rb
+++ app/packages/education/controllers/education_admin/education_categories_controller.rb
@@ -2,10 +2,10 @@ class EducationAdmin::EducationCategoriesController < EducationAdmin::Educations
 
   # GET /education_categories or /education_categories.json
   def index
-    @education_categories = EducationCategory.where(education_school: @education_schools)
+    @education_categories = EducationCategory.where(user: current_user.education_admins.first.education_schools.first.user)
     @pagination, @education_categories = pagy(@education_categories)
     @data = {
-      education_categories: @education_categories.as_json(only: %i[id name education_school_id parent_category_id created_at updated_at], include: { education_school: { only: %i[id name] } }),
+      education_categories: @education_categories.as_json(only: %i[id name created_at updated_at]),
     }.to_json
   end
 
