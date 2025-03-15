a5e47d5f09ece3473f73911b75791d4b55130ae9
skyler <pkphung2001@gmail.com>
2025-03-15 18:15:52 +0700

    update

---
+++ spec/features/education/role/education_school/education_students/import_view_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "education_students#import_view", type: :feature, js: true do
+  include_context "support/shared_contexts/education/default_database"
+  
+  context "education_role: :education_school" do
+    it "will not be redirected" do
+      debugger
+      sign_in(user: education_school.user)
+      visit import_view_education_students_path
+      # debugger   
+      expect(page).to have_current_path(import_view_education_students_path, ignore_query: true)
+      expect(page).to have_content("Click to upload")
+      expect(page).to have_content(" or drag and dropV")
+      expect(page).to have_content("Support: CSV")
+    end
+  end
+
+  context "education_role: :not_education_user" do
+    before do
+      education_school.user.update(education_role: nil)
+    end
+
+    it "will be redirected" do
+      sign_in(user: education_school.user)
+      visit import_view_education_students_path
+      expect(page).to have_routing_error
+    end
+  end
+end
