271627b9f6b636e0f9bebc9af7765e2c435db88b
skyler <pkphung2001@gmail.com>
2025-03-18 01:37:13 +0700

    update

--- app/packages/education/controllers/education_school/calendar_events_controller.rb
+++ app/packages/education/controllers/education_school/calendar_events_controller.rb
@@ -8,4 +8,19 @@ class EducationSchool::CalendarEventsController < EducationSchool::EducationsCon
       calendar_events: @calendar_events.as_json,
     }.to_json
   end
+
+  def create
+    debugger
+    @calendar_event = CalendarEvent.new(calendar_event_params)
+
+    respond_to do |format|
+      if @calendar_event.save
+        format.html { redirect_to @calendar_event, notice: "Calendar event was successfully created." }
+        format.json { render :show, status: :created, location: @calendar_event }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @calendar_event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
