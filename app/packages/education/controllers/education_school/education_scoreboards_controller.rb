36fca243cc7e3e21ccc146ae03aae86bf2414b1d
skyler <pkphung2001@gmail.com>
2025-03-31 19:04:47 +0700

    update

--- app/packages/education/controllers/education_school/education_scoreboards_controller.rb
+++ app/packages/education/controllers/education_school/education_scoreboards_controller.rb
@@ -1,13 +1,15 @@
 class EducationSchool::EducationScoreboardsController < EducationSchool::EducationsController
   def index
-    @education_classes_for_select = EducationClass.where(education_school: @education_schools)
+    @education_classes_for_select = Rails.cache.fetch("education_classes_for_select/#{@education_schools.cache_key_with_version}", expires_in: 5.minutes) do
+      EducationClass.where(education_school: @education_schools).to_a
+    end
     # @education_subjects_for_select = EducationSubject.where(education_school: @education_schools)
 
-    @education_classes = EducationClass.where(id: params[:education_class_id])
+    @education_classes = EducationClass.joins(:education_subjects).where(id: params[:education_class_id])
     @education_subjects = EducationSubject.where(id: params[:education_subject_id])
     @education_students = EducationStudent.joins(:education_classes).where(education_classes: @education_classes)
     @education_exams = EducationExam.joins(:education_classes).where(education_classes: @education_classes, education_subject: @education_subjects)
-    @education_exam_appointments = EducationExamAppointment.joins(:education_exam).where(education_exam: @education_exams)
+    @education_exam_appointments = EducationExamAppointment.joins(:education_exam).where(education_exam: @education_exams, appoint_to: @education_students)
     @pagination, @education_students = pagy(@education_students)
     @data = {
       education_students: @education_students.as_json,
