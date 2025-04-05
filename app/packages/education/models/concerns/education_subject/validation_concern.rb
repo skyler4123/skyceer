a179969c14c45f755b56e6eee5261520eec512ab
skyler <pkphung2001@gmail.com>
2025-04-05 21:34:39 +0700

    update

--- app/packages/education/models/concerns/education_subject/validation_concern.rb
+++ app/packages/education/models/concerns/education_subject/validation_concern.rb
@@ -2,10 +2,8 @@ module EducationSubject::ValidationConcern
   extend ActiveSupport::Concern
   
   included do
-    validates :name, presence: true, length: { maximum: 255 }
-    validates :description, presence: true, length: { maximum: 255 }
-    validates :education_school, presence: true
-    validates :name, uniqueness: { scope: :education_school_id, message: "must be unique within the same education school" }
+    validates :name, presence: true, uniqueness: { scope: :education_school_id, message: "must be unique within the same school" }
+    validates :description, presence: true
   end
 
 end
--- spec/models/education_subject_spec.rb
+++ spec/models/education_subject_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe EducationSubject, type: :model do
-  let(:education_school) { create(:education_school) }
-  let(:education_subject) { build(:education_subject, education_school: education_school) }
-
   describe 'associations' do
     it { should belong_to(:education_school) }
   end
 
   describe 'validations' do
+    subject { build(:education_subject) }
+
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
+
+    it do
+      should validate_uniqueness_of(:name)
+        .scoped_to(:education_school_id)
+        .with_message("must be unique within the same school")
+    end
   end
 end
