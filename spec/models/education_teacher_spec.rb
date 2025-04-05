7fef790264f05adadbc3dc45640cb35ff0b8ef18
skyler <pkphung2001@gmail.com>
2025-04-05 21:13:28 +0700

    update

--- spec/models/education_teacher_spec.rb
+++ spec/models/education_teacher_spec.rb
@@ -1,15 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe EducationTeacher, type: :model do
-  # Association tests
-  it { should belong_to(:user).optional }
-  it { should have_many(:education_school_appointments).dependent(:destroy) }
-  it { should have_many(:education_schools).through(:education_school_appointments) }
-  it { should have_many(:education_class_appointments).dependent(:destroy) }
-  it { should have_many(:education_classes).through(:education_class_appointments) }
+  describe 'associations' do
+    it { should belong_to(:user).optional }
+    it { should belong_to(:education_school_user).class_name('User') }
+    it { should belong_to(:user).optional }
+    it { should have_many(:education_school_appointments).dependent(:destroy) }
+    it { should have_many(:education_schools).through(:education_school_appointments) }
+    it { should have_many(:education_class_appointments).dependent(:destroy) }
+    it { should have_many(:education_classes).through(:education_class_appointments) }
+  end
 
-  # Validation tests
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:email) }
+  describe 'validations' do
+    subject { build(:education_teacher) }
 
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+
+    it do
+      should validate_uniqueness_of(:name)
+        .scoped_to(:education_school_user_id)
+        .with_message("must be unique within the same school user")
+    end
+
+    it do
+      should validate_uniqueness_of(:email)
+        .with_message("must be unique")
+    end
+  end
 end
