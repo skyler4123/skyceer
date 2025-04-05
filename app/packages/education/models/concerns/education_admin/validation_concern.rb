41a143bc8b27d8f77af35d3a3532df6428809e87
skyler <pkphung2001@gmail.com>
2025-04-05 20:32:12 +0700

    update

--- app/packages/education/models/concerns/education_admin/validation_concern.rb
+++ app/packages/education/models/concerns/education_admin/validation_concern.rb
@@ -2,8 +2,8 @@ module EducationAdmin::ValidationConcern
   extend ActiveSupport::Concern
   
   included do
-    validates :name, presence: true, length: { maximum: 255 }
-    validates :email, presence: true, length: { maximum: 255 }
+    validates :name, presence: true, uniqueness: { scope: :education_school_user_id, message: "must be unique within the same school user" }
+    validates :email, presence: true, uniqueness: { case_sensitive: true, message: "must be unique" }
   end
 
 end
