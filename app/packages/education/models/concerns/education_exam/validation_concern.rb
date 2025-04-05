2c893d84f3c3c94272928f50c130d1925ee975c8
skyler <pkphung2001@gmail.com>
2025-04-05 21:53:20 +0700

    update

--- app/packages/education/models/concerns/education_exam/validation_concern.rb
+++ app/packages/education/models/concerns/education_exam/validation_concern.rb
@@ -2,8 +2,8 @@ module EducationExam::ValidationConcern
   extend ActiveSupport::Concern
   
   included do
-    validates :name, presence: true, length: { maximum: 255 }
-    validates :education_school, presence: true
+    validates :name, presence: true, uniqueness: { scope: :education_school_id, message: "must be unique within the same school" }
+    validates :description, presence: true
   end
 
 end
