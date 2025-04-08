66e264e51bf8515833544a734fd2d83d687d77a0
skyler <pkphung2001@gmail.com>
2025-04-08 07:49:38 +0700

    update

--- app/packages/education/controllers/education_student/education_teachers_controller.rb
+++ app/packages/education/controllers/education_student/education_teachers_controller.rb
@@ -4,10 +4,11 @@ class EducationStudent::EducationTeachersController < EducationStudent::Educatio
 
   # GET /education_teachers or /education_teachers.json
   def index
-    @education_teachers = @education_classes.education_teachers
+    # @education_teachers = @education_classes.education_teachers
+    @education_teachers = EducationTeacher.joins(:education_classes).where(education_classes: @education_classes)
     @pagination, @education_teachers = pagy(@education_teachers)
     @data = {
-      education_teachers: @education_teachers.as_json(include: [:education_schools, :education_classes])
+      education_teachers: @education_teachers.as_json(include: [:education_schools, :education_classes, :education_subjects])
     }.to_json
   end
 
