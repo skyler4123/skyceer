82953ff414756e4d66da05b50abb7faa30c7a48e
skyler <pkphung2001@gmail.com>
2025-05-08 02:23:45 +0700

    update

---
+++ spec/gems/elasticsearch_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'External Service Connections - Elasticsearch' do
+  describe 'Elasticsearch Connection' do
+    it 'connects to Elasticsearch successfully' do
+      expect { Elasticsearch::Model.client.ping }.not_to raise_error
+      expect(Elasticsearch::Model.client.ping).to be true
+    end
+  end
+end
---
+++ spec/gems/mongoid_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'External Service Connections - MOngoDB' do
+  describe 'MongoDB Connection' do
+    it 'connects to the MongoDB database successfully' do
+      expect { Mongoid::Clients.default.database.name }.not_to raise_error
+      expect(Mongoid::Clients.default.database.name).to eq(ENV.fetch('MONGO_HOST', 'skyler_development'))
+    end
+  end
+end
