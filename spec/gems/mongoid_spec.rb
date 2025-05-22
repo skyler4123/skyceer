22db835aa4b1765d4942132c31140898116c820a
skyler <pkphung2001@gmail.com>
2025-05-22 18:00:17 +0700

    update

--- spec/gems/mongoid_spec.rb
+++ spec/gems/mongoid_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'External Service Connections - MOngoDB' do
   describe 'MongoDB Connection' do
     it 'connects to the MongoDB database successfully' do
       expect { Mongoid::Clients.default.database.name }.not_to raise_error
-      expect(Mongoid::Clients.default.database.name).to eq(ENV.fetch('MONGO_HOST', 'skyler_development'))
+      expect(Mongoid::Clients.default.database.name).to eq("skyceer_test")
     end
   end
 end
