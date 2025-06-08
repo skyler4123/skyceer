b648578e3938a140128ac9280c298d363a676c94
skyler <pkphung2001@gmail.com>
2025-06-09 05:33:39 +0700

    test failed test on github action

--- spec/features/demo_spec.rb
+++ spec/features/demo_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "Demo", type: :feature do
-  it "will not be redirected" do
+  it "will passed" do
     expect(true).to be_truthy
     # expect(true).to be_falsey
+  end
+
+  it "will failed" do
+    expect(true).to be_falsey
   end
 end
