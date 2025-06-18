d9e56d1dad1b420022407eca00b3b946ee40e048
skyler <pkphung2001@gmail.com>
2025-06-18 21:56:27 +0700

    update

--- spec/gems/elasticsearch_spec.rb
+++ spec/gems/elasticsearch_spec.rb
@@ -1,10 +1,10 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'External Service Connections - Elasticsearch' do
-  describe 'Elasticsearch Connection' do
-    it 'connects to Elasticsearch successfully' do
-      expect { Elasticsearch::Model.client.ping }.not_to raise_error
-      expect(Elasticsearch::Model.client.ping).to be true
-    end
-  end
-end
+# RSpec.describe 'External Service Connections - Elasticsearch' do
+#   describe 'Elasticsearch Connection' do
+#     it 'connects to Elasticsearch successfully' do
+#       expect { Elasticsearch::Model.client.ping }.not_to raise_error
+#       expect(Elasticsearch::Model.client.ping).to be true
+#     end
+#   end
+# end
