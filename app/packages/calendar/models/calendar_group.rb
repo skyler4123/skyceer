412d3320ec560c7c4769b6000ce1cf4f85e8bd89
skyler <pkphung2001@gmail.com>
2025-07-27 08:12:54 +0700

    update

--- app/packages/calendar/models/calendar_group.rb
+++ app/packages/calendar/models/calendar_group.rb
@@ -5,4 +5,9 @@ class CalendarGroup < ApplicationRecord
   has_many :calendar_categories, through: :calendar_category_appointments
 
   has_many :calendar_events, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: { scope: :calendar_owner_id }
+  # validates :status, inclusion: { in: [0, 1, 2], message: "%{value} is not a valid status" }
+  # validates :position, numericality: { greater_than_or_equal_to: 0 }
+  validates :color, format: { with: /\A#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})\z/, message: "must be a valid hex color" }
 end
