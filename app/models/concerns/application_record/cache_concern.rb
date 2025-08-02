be7019b14310386244792017f66aa8a54ff914b1
skyler <pkphung2001@gmail.com>
2025-08-02 18:17:49 +0700

    update

--- app/models/concerns/application_record/cache_concern.rb
+++ app/models/concerns/application_record/cache_concern.rb
@@ -3,11 +3,21 @@ module ApplicationRecord::CacheConcern
 
   included do
     def cache_key
-      "#{self.class.name.downcase}/#{id}-#{updated_at.to_i}"
+      "#{self.class.name.downcase}_#{id}"
     end
 
-    def cache!
-      Rails.cache.write(cache_key, self)
+    def cache!(expires_in: nil)
+      expires_at = self.try(:expires_at) || Time.current + 5.minutes
+      expires_in = expires_at - Time.current
+
+      cache_key = self.cache_key
+      Rails.cache.write(cache_key, self, expires_in: expires_in)
+    end
+
+    def self.fetch_from_cache(id)
+      Rails.cache.fetch("#{name.downcase}_#{id}") do
+        find(id)
+      end
     end
   end
 end
