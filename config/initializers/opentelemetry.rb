20e61ee3db2642ce4139a13d1acd49f5cb0adb5c
skyler <pkphung2001@gmail.com>
2025-08-24 17:25:49 +0700

    update

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -1,39 +1,39 @@
-# config/initializers/opentelemetry.rb
-require "opentelemetry/sdk"
-require "opentelemetry/exporter/otlp"
-require "opentelemetry/instrumentation/all"
+# # config/initializers/opentelemetry.rb
+# require "opentelemetry/sdk"
+# require "opentelemetry/exporter/otlp"
+# require "opentelemetry/instrumentation/all"
 
-ENV["OTEL_EXPORTER_OTLP_ENDPOINT"] = Rails.application.credentials.dig(:otel_exporter_otlp_endpoint) || "http://192.168.0.100:4318"
+# ENV["OTEL_EXPORTER_OTLP_ENDPOINT"] = Rails.application.credentials.dig(:otel_exporter_otlp_endpoint) || "http://192.168.0.100:4318"
 
-OpenTelemetry::SDK.configure do |c|
-  c.service_name = "skyceer-#{Rails.env}"
+# OpenTelemetry::SDK.configure do |c|
+#   c.service_name = "skyceer-#{Rails.env}"
 
-  # c.add_span_processor(
-  #   OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
-  #     OpenTelemetry::Exporter::OTLP::Exporter.new(
-  #       # "monitor:4318" is the container name for the OTLP endpoint
-  #       endpoint: Rails.application.credentials.dig(:opentelemetry_traces_endpoint) || 'http://localhost:4318/v1/traces',
-  #       timeout: 30
-  #     ),
-  #     # Prevent rapid-fire exports that cause recursion
-  #     schedule_delay: 5000,      # 5 seconds between exports
-  #     max_queue_size: 2048,      # Larger queue
-  #     max_export_batch_size: 512 # Smaller batches
-  #   )
-  # )
+#   # c.add_span_processor(
+#   #   OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
+#   #     OpenTelemetry::Exporter::OTLP::Exporter.new(
+#   #       # "monitor:4318" is the container name for the OTLP endpoint
+#   #       endpoint: Rails.application.credentials.dig(:opentelemetry_traces_endpoint) || 'http://localhost:4318/v1/traces',
+#   #       timeout: 30
+#   #     ),
+#   #     # Prevent rapid-fire exports that cause recursion
+#   #     schedule_delay: 5000,      # 5 seconds between exports
+#   #     max_queue_size: 2048,      # Larger queue
+#   #     max_export_batch_size: 512 # Smaller batches
+#   #   )
+#   # )
 
-  # Use all instrumentation BUT exclude Net::HTTP for OTLP endpoint
-  c.use_all({
-    "OpenTelemetry::Instrumentation::Net::HTTP" => {
-      enabled: false, # Disable Net::HTTP instrumentation
-      # This prevents recursion when the OTLP exporter tries to send traces
-      # to the same host that is being instrumented.
-      # If you need to trace HTTP requests, consider using a different host or port.
-      # This is a workaround to avoid recursion issues with OTLP exports.
-      # You can also use a different exporter or disable tracing for specific hosts.
-      # Example: if you have a monitoring service running on 'monitor:4318',
-      # you can add it to untraced_hosts.
-      untraced_hosts: [ "monitor:4318", "localhost:4318" ] # Don't trace OTLP exports
-    }
-  })
-end
+#   # Use all instrumentation BUT exclude Net::HTTP for OTLP endpoint
+#   c.use_all({
+#     "OpenTelemetry::Instrumentation::Net::HTTP" => {
+#       enabled: false, # Disable Net::HTTP instrumentation
+#       # This prevents recursion when the OTLP exporter tries to send traces
+#       # to the same host that is being instrumented.
+#       # If you need to trace HTTP requests, consider using a different host or port.
+#       # This is a workaround to avoid recursion issues with OTLP exports.
+#       # You can also use a different exporter or disable tracing for specific hosts.
+#       # Example: if you have a monitoring service running on 'monitor:4318',
+#       # you can add it to untraced_hosts.
+#       untraced_hosts: [ "monitor:4318", "localhost:4318" ] # Don't trace OTLP exports
+#     }
+#   })
+# end
