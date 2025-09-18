80a65388e8e5cde474c5a01a0443b106291c4f97
skyler <pkphung2001@gmail.com>
2025-09-19 00:34:21 +0700

    update

--- spec/capybara_helper.rb
+++ spec/capybara_helper.rb
@@ -1,12 +1,15 @@
 require 'capybara'
 require 'selenium-webdriver'
 
-Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-Capybara.javascript_driver = :selenium_chrome
-Capybara.default_max_wait_time = 10
+# Capybara.register_driver :selenium_chrome do |app|
+#   Capybara::Selenium::Driver.new(app, browser: :chrome)
+# end
+# Capybara.javascript_driver = :selenium_chrome
+# Capybara.default_max_wait_time = 10
 
 # Capybara.default_driver = :selenium_chrome
 # Capybara.use_default_driver
 # Capybara.app_host = "http://" + 'localhost:3000'
+
+Capybara.javascript_driver = :selenium_chrome_headless
+
