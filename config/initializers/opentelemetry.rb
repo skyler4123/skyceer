95df72f183bee2d834180071c96277f4ce7f3f00
skyler <pkphung2001@gmail.com>
2025-10-18 09:47:54 +0700

    minor

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -9,19 +9,19 @@ if Rails.env.production?
   OpenTelemetry::SDK.configure do |c|
     c.service_name = "skyceer-#{Rails.env}"
 
-    # c.add_span_processor(
-    #   OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
-    #     OpenTelemetry::Exporter::OTLP::Exporter.new(
-    #       # "monitor:4318" is the container name for the OTLP endpoint
-    #       endpoint: Rails.application.credentials.dig(:opentelemetry_traces_endpoint) || 'http://localhost:4318/v1/traces',
-    #       timeout: 30
-    #     ),
-    #     # Prevent rapid-fire exports that cause recursion
-    #     schedule_delay: 5000,      # 5 seconds between exports
-    #     max_queue_size: 2048,      # Larger queue
-    #     max_export_batch_size: 512 # Smaller batches
-    #   )
-    # )
+#     # c.add_span_processor(
+#     #   OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
+#     #     OpenTelemetry::Exporter::OTLP::Exporter.new(
+#     #       # "monitor:4318" is the container name for the OTLP endpoint
+#     #       endpoint: Rails.application.credentials.dig(:opentelemetry_traces_endpoint) || 'http://localhost:4318/v1/traces',
+#     #       timeout: 30
+#     #     ),
+#     #     # Prevent rapid-fire exports that cause recursion
+#     #     schedule_delay: 5000,      # 5 seconds between exports
+#     #     max_queue_size: 2048,      # Larger queue
+#     #     max_export_batch_size: 512 # Smaller batches
+#     #   )
+#     # )
 
     # Use all instrumentation BUT exclude Net::HTTP for OTLP endpoint
     c.use_all({
@@ -34,7 +34,7 @@ if Rails.env.production?
         # You can also use a different exporter or disable tracing for specific hosts.
         # Example: if you have a monitoring service running on 'monitor:4318',
         # you can add it to untraced_hosts.
-        untraced_hosts: [ "monitor:4318", "grafana:4318", "localhost:4318" ] # Don't trace OTLP exports
+        untraced_hosts: [ "grafana:4318", "localhost:4318" ] # Don't trace OTLP exports
       }
     })
   end
