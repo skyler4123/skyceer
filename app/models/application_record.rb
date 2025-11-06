2615d6d3182c1216d1f5f3cd66b25b5557fa0133
skyler <pkphung2001@gmail.com>
2025-11-06 21:24:09 +0700

    fix

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
-  include Pagy::Backend
+  # include Pagy::Backend
   include ApplicationRecord::ImageConcern
   include ApplicationRecord::SearchkickConcern
   include ApplicationRecord::CacheConcern
